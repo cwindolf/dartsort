639bb290d2b2b1f5171482563cd8182821029327
Charlie Windolf <charliexwindolf@gmail.com>
2021-10-27 18:39:31 -0400

    float

--- spike_psvae/torch_utils.py
+++ spike_psvae/torch_utils.py
@@ -1,5 +1,6 @@
 import h5py
 import numpy as np
+import torch
 from torch.utils.data import Dataset
 
 
@@ -8,14 +9,20 @@ class SpikeHDF5Dataset(Dataset):
     def __init__(self, h5_path, x, ys):
         self.h5 = h5py.File(h5_path, "r")
         self.x = self.h5[x]
-        self.ys = np.stack(
-            [self.h5[y][:] for y in ys],
-            axis=1,
+        self.ys = torch.tensor(
+            np.stack(
+                [self.h5[y][:].astype(np.float32) for y in ys],
+                axis=1,
+            )
         )
+
         self.len = len(self.ys[0])
 
     def __len__(self):
         return self.len
 
     def __getitem__(self, idx):
-        return self.x[idx], self.ys[idx]
+        return (
+            torch.tensor(self.x[idx], dtype=torch.float),
+            self.ys[idx]
+        )
