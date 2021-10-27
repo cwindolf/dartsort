3c086480eeb5febffc82030a6fc7259597342dbe
Charlie Windolf <charliexwindolf@gmail.com>
2021-10-27 14:44:32 -0400

    initial model

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # spike-psvae
+
+*Getting started.* Install `node` without conda, can't figure out how to avoid an environment conflict. Then,
+
+```
+conda create -n psvae python=3.8 numpy seaborn scikit-image scikit-learn scipy h5py tqdm
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
+```
+
+For jupyter lab, the way I'm doing it for now is:
+
+```
+pip install jupyterlab jupytext
+jupyter lab build
+```
+
+Then serve:
+
+```
+mkdir -p .jupter/lab/workspaces
+JUPYTERLAB_WORKSPACES_DIR=.jupyter/lab/workspaces jupyter lab --no-browser --ip=0.0.0.0
+```
---
+++ spike_psvae/psvae.py
@@ -0,0 +1,111 @@
+# helpful code references:
+# github.com/AntixK/PyTorch-VAE/
+# github.com/themattinthehatt/behavenet/blob/master/behavenet/models/vaes.py
+# github.com/pytorch/examples/blob/master/vae/main.py
+
+import torch
+import torch.utils.data
+from torch import nn
+from torch.nn import functional as F
+
+from . import layers
+
+
+class PSVAE(nn.Module):
+    def __init__(
+        self,
+        encoder,
+        decoder,
+        supervised_latent_dim,
+        unsupervised_latent_dim,
+        alpha=1.0,
+    ):
+        """
+        Encoder
+        """
+        assert encoder.input_dim == decoder.output_dim
+        assert encoder.output_dim == decoder.input_dim
+        latent_dim = unsupervised_latent_dim + supervised_latent_dim
+        assert encoder.output_dim == latent_dim
+
+        self.input_dim = encoder.input_dim
+        self.latent_dim = latent_dim
+        self.unsupervised_latent_dim = unsupervised_latent_dim
+        self.supervised_latent_dim = supervised_latent_dim
+
+        self.alpha = alpha
+
+        self.encoder = encoder
+        self.decoder = decoder
+
+        self.fc_mu = nn.Linear(latent_dim, latent_dim)
+        self.fc_logvar = nn.Linear(latent_dim, latent_dim)
+        self.diag_y_hat = layers.DiagLinear(supervised_latent_dim)
+
+    def encode(self, x):
+        h = self.encoder(x)
+        return self.fc_mu(h), self.fc_logvar(h)
+
+    def reparametrize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        z = mu + eps * std
+        return z
+
+    def split(self, z):
+        # TODO diagonal matrix multiply?
+        zs = z[: self.supervised_latent_dim]
+        zu = z[self.supervised_latent_dim :]
+        return zs, zu
+
+    def decode(self, z):
+        # TODO might need to add an activation?
+        return self.decoder(z)
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparametrize(mu, logvar)
+        zs, zu = self.split(z)
+        y_hat = self.diag_y_hat(zs)
+        recon_x = self.decode(z)
+        return recon_x, y_hat, mu, logvar
+
+    def loss(self, x, y, recon_x, y_hat, mu, logvar):
+        # mean over batches, sum over data dims
+
+        # TODO is the distribution of latents different in PSVAE?
+        #      like, supervised have their own mean?
+        # -KL divergence to iid standard normal
+        # 1312.6114 appendix B
+        # note, -DKL is in ELBO which we want to maximize
+        # here we are minimizing, so take just DKL
+        # we omit the factor of 1/2 here and in the reconstruction
+        # error
+        dkl = torch.mean((mu.pow(2) + logvar.exp() - 1 - logvar).sum(axis=1))
+
+        # reconstruction error -- conditioned gaussian log likelihood
+        # we make the "variational assumption" that p(x | z) has std=1
+        # so that the only relevant term is the mse (after omitting the
+        # half as above)
+        mse_recon_full = F.mse_loss(x, recon_x, reduction="none")
+        mse_recon = torch.mean(
+            torch.sum(
+                mse_recon_full,
+                axis=tuple(range(1, mse_recon_full.ndim)),
+            )
+        )
+
+        # supervised loss
+        mse_labels_full = F.mse_loss(y, y_hat, reduction="none")
+        mse_labels = torch.mean(
+            torch.sum(
+                mse_labels_full,
+                axis=tuple(range(1, mse_labels_full.ndim)),
+            )
+        )
+
+        # TODO total correlation? beta annealing?
+        #      unsupervised latents index-code mutual information?
+
+        loss = dkl + mse_recon + self.alpha * mse_labels
+        return loss
