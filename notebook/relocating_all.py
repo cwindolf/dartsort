5564b71f08ee6d760d69d0c75aae218c889d1999
Charlie Windolf <c.windolf@columbia.edu>
2021-11-11 16:33:59 -0500

    prep merge

--- notebook/relocating_all.py
+++ notebook/relocating_all.py
@@ -5,9 +5,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.13.1
+#       jupytext_version: 1.13.0
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
@@ -28,17 +28,20 @@ from scipy import linalg
 plt.rc("figure", dpi=200)
 
 # %%
-with h5py.File("../data/wfs_locs_tiny.h5") as f:
+with h5py.File("../data/wfs_locs.h5") as f:
     y = f["y"][:]
-    good = np.flatnonzero(y >= 1)
+    maxchans = f["max_channels"][:]
+    good = np.flatnonzero((y >= 1) & (maxchans > 10))[:10000]
+    print(len(good))
     y = y[good]
+    maxchans = maxchans[good]
     wfs = f["denoised_waveforms"][good]
     x = f["x"][good]
     z = f["z_rel"][good]
     z_abs = f["z"][good]
     alpha = f["alpha"][good]
     max_ptp = f["max_ptp"][good]
-    maxchans = f["max_channels"][good]
+
 
 # %%
 plt.plot(x, z_abs, "k.", ms=1);
@@ -92,6 +95,9 @@ plt.legend()
 plt.show()
 
 # %%
+maxchans[0], maxchans[4447], maxchans[4448], maxchans[4449]
+
+# %%
 batch = torch.tensor(wfs[:])
 bx = torch.tensor(x[:])
 by = torch.tensor(y[:])
@@ -102,11 +108,45 @@ balpha = torch.tensor(alpha[:])
 reloc = point_source_centering.relocate_simple(batch, geom, bmaxchan, bx, by, bz, balpha)
 
 # %%
-vals = linalg.svdvals(batch.numpy().reshape(614, -1))
+vals = linalg.svdvals(batch.numpy().reshape(10000, -1))
 (np.cumsum(vals) / np.sum(vals) < 0.95).sum()
 
 # %%
-vals = linalg.svdvals(reloc.numpy().reshape(614, -1))
-(np.cumsum(vals) / np.sum(vals) < 0.95).sum()
+vals = linalg.svdvals(reloc.numpy().reshape(10000, -1))
+(np.cumsum(vals) / np.sum(vals) < 0.90).sum()
+
+# %%
+np.arange(35).reshape(5, 7).mean(axis=1).shape
+
+
+# %%
+def nf(x):
+    x = x.numpy().reshape(10000, -1)
+    x -= x.mean(axis=1, keepdims=True)
+    eigs = np.square(linalg.svdvals(x))
+    return eigs
+
+
+# %%
+eigs_n = nf(batch)
+
+# %%
+eigs_y = nf(reloc)
+
+# %%
+(np.cumsum(eigs_n) / eigs_n.sum() < 0.95).sum()
+
+# %%
+(np.cumsum(eigs_y) / eigs_y.sum() < 0.95).sum()
+
+# %%
+
+# %%
+plt.plot(np.log(eigs_n)[:100])
+plt.plot(np.log(eigs_y)[:100])
+
+# %%
+plt.plot(np.cumsum(eigs_n)[:100] / eigs_n.sum())
+plt.plot(np.cumsum(eigs_y)[:100] / eigs_y.sum())
 
 # %%
