c054d96510653ac17c1bc592d664226ca835b818
Charlie Windolf <charliexwindolf@gmail.com>
2021-11-16 15:55:17 -0500

    parafac errors

--- spike_psvae/decomp.py
+++ spike_psvae/decomp.py
@@ -1,10 +1,11 @@
 import numpy as np
 from tensorly.decomposition import parafac
+from tqdm.auto import trange
 
 
 def cumparafac(wfs, max_rank=100):
     errors = []
-    for rank in range(max_rank):
+    for rank in trange(1, max_rank):
         cptensor, _ = parafac(wfs, rank)
         rec = np.einsum("l,il,jl,kl->ijk", cptensor.weights, *cptensor.factors)
         mse = np.square(wfs - rec).sum(axis=(1, 2)).mean()
