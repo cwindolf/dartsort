d30daedbd726245e1f56e0313aa5aecfcd99da83
Charlie Windolf <cwindolf95@gmail.com>
2021-12-09 14:17:14 -0500

    Progress on presentation

--- notebook/storytelling.py
+++ notebook/storytelling.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.13.0
+#       jupytext_version: 1.13.3
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -19,10 +19,13 @@
 # %%
 import numpy as np
 import h5py
+import matplotlib
 import matplotlib.pyplot as plt
 import scipy.linalg as la
 import seaborn as sns
 import time
+from tqdm.auto import tqdm, trange
+from sklearn.decomposition import IncrementalPCA
 
 # %%
 from npx import lib, reg, cuts
@@ -31,7 +34,10 @@ from npx import lib, reg, cuts
 from isosplit import isosplit
 
 # %%
-from spike_psvae import waveform_utils, localization, point_source_centering, vis_utils
+from spike_psvae import waveform_utils, localization, point_source_centering, vis_utils, statistics
+
+# %%
+sns.set_style("ticks")
 
 # %%
 rg = lambda k=0: np.random.default_rng(k)
@@ -44,14 +50,13 @@ plt.rc("figure", dpi=200)
 
 # %%
 # single channel denoised data
-original_h5 = "../data/wfs_locs_c.h5"
+original_h5 = "../data/wfs_locs_b.h5"
 
 # %%
 # we will write some waveforms data here
-full_h5 = "../data/story_full_c.h5"
 # this one will just have features, cluster ids, lightweight stuff
 # that can be rsyncd to local for datoviz
-feats_h5 = "../data/story_feats_c.h5"
+feats_h5 = "../data/story_feats_b.h5"
 
 
 # %% [markdown]
@@ -78,16 +83,21 @@ class timer:
 
 # %%
 with h5py.File(original_h5, "r") as orig_f:
+    ckw = dict(geomkind="updown")
+    if "first_channels" in orig_f:
+        ckw = dict(geomkind="firstchan", firstchans=orig_f["first_channels"][:])
+        
+    N, T, C = orig_f["denoised_waveforms"].shape
+    
     xs, ys, z_rels, z_abss, alphas = localization.localize_waveforms_batched(
         orig_f["denoised_waveforms"],
         orig_f["geom"][:],
         maxchans=orig_f["max_channels"][:],
         channel_radius=10,
-        n_workers=10,
+        n_workers=8,
         jac=True,
-        firstchans=orig_f["first_channels"][:],
-        geomkind="firstchan",
-        batch_size=1024,
+        batch_size=2048,
+        **ckw,
     )
 
 # %%
@@ -96,7 +106,7 @@ with h5py.File(original_h5, "r") as orig_f:
     with timer("maxptp"):
         maxptp = orig_f["denoised_waveforms"][:].ptp(1).ptp(1)
 
-# %%
+# %% tags=[]
 with timer("registration"):
     z_reg, dispmap = reg.register_nonrigid(
         maxptp,
@@ -117,10 +127,21 @@ R0, _, _ = lib.faster(maxptp, z_abss, times)
 Rreg, _, _ = lib.faster(maxptp, z_reg, times)
 
 # %%
-fig, (aa, ab) = plt.subplots(1, 2, figsize=(5, 10))
+fig, (aa, ab) = plt.subplots(2, 1, figsize=(5, 10))
 cuts.plot(R0, ax=aa)
 cuts.plot(Rreg, ax=ab)
 plt.tight_layout()
+plt.show()
+
+# %%
+z_disp = z_reg - (z_abss - z_abss.min())
+fig, ax = plt.subplots()
+a = ax.scatter(times, z_reg, c=z_disp, s=0.5, alpha=0.5)
+cbar = fig.colorbar(a, ax=ax)
+cbar.solids.set_alpha(1)
+plt.title("Spikes colored by displacement")
+plt.ylabel("registered depth")
+plt.xlabel("time")
 plt.show()
 
 # %%
@@ -147,10 +168,8 @@ with h5py.File(feats_h5, "w") as feats_f:
 # ## relocation
 
 # %%
-with h5py.File(feats_h5, "r") as feats_f:
-    N = len(feats_f["x"])
 # 9 random spike indices
-example_inds = rg().choice(N, 9)
+example_inds = rg(1).choice(N, 9)
 example_inds.sort()
 
 # %%
@@ -166,18 +185,330 @@ ex_zr = z_rels[example_inds]
 ex_a = alphas[example_inds]
 
 # %%
-vis_utils.labeledmosaic(exwf.reshape(3, 3, 121, 20)[:, :, 20:-40, :], [1, 2, 3], pad=1);
+vis_utils.labeledmosaic(exwf.reshape(3, 3, T, C)[:, :, 15:, :], [1, 2, 3], pad=1);
 
 # %%
-p = exwf.ptp(1)
-exwf_yza, q_hat_yza, p_hat = point_source_centering.relocate_simple(
+exp = exwf.ptp(1)
+exwf_yza, ex_q_hat_yza, ex_p_hat = point_source_centering.relocate_simple(
     exwf, geom, exmc, ex_x, ex_y, ex_zr, ex_a,
     relocate_dims="yza",
 )
-exwf_xyza, q_hat_xyza, p_hat_ = point_source_centering.relocate_simple(
+exwf_xyza, ex_q_hat_xyza, ex_p_hat_ = point_source_centering.relocate_simple(
     exwf, geom, exmc, ex_x, ex_y, ex_zr, ex_a,
-    relocate_dims="yza",
+    relocate_dims="xyza",
 )
-assert (p_hat == p_hat_).all()
+assert (ex_p_hat == ex_p_hat_).all()
+
+# %%
+ex_q_yza = exwf_yza.numpy().ptp(1)
+ex_q_xyza = exwf_xyza.numpy().ptp(1)
+
+# %%
+fig, axes = vis_utils.vis_ptps(
+    [exp, ex_p_hat.numpy()], ["observed ptp", "predicted ptp"], "bg"
+)
+plt.tight_layout(pad=0.25)
+plt.show()
+fig, axes = vis_utils.vis_ptps(
+    [ex_q_yza, ex_q_hat_yza.numpy()], ["yza relocated ptp", "yza standard ptp"], "kr"
+)
+plt.tight_layout(pad=0.25)
+plt.show()
+fig, axes = vis_utils.vis_ptps(
+    [ex_q_xyza, ex_q_hat_xyza.numpy()], ["xyza relocated ptp", "xyza standard ptp"], "kr"
+)
+plt.tight_layout(pad=0.25)
+plt.show()
+
+# %%
+vis_utils.labeledmosaic(
+    [exwf[:, 15:, :], exwf_yza[:, 15:, :], exwf_xyza[:, 15:, :]],
+    ["original", "yza reloc", "xyza reloc"],
+    pad=1,
+)
+
+
+# %%
+
+# %% [markdown]
+# ## PCA
+
+# %% [markdown]
+# ### test: PCA error on a batch of data
+
+# %%
+def pca_resid_plot(wfs, ax=None, q=0.95, c="b", name=None, pad=0, K=25):
+    wfs = wfs.reshape(wfs.shape[0], -1)
+    wfs = wfs - wfs.mean(axis=0, keepdims=True)
+    v = np.square(la.svdvals(wfs)[:K - pad]) / np.prod(wfs.shape)
+    ax = ax or plt.gca()
+    totvar = np.square(wfs).mean()
+    residvar = np.concatenate(([totvar], totvar - np.cumsum(v)))
+    if pad:
+        ax.plot(([totvar] * pad + [*residvar]), marker=".", c=c, label=name)
+    else:
+        ax.plot(residvar[:50], marker=".", c=c, label=name)
+
+
+# %%
+B = 50_000
+with h5py.File(original_h5, "r") as orig_f:
+    batch_wfs = orig_f["denoised_waveforms"][:B]
+    batch_mc = orig_f["max_channels"][:B]
+    
+    batch_wfs_yza, q_hat_yza, p_hat = point_source_centering.relocate_simple(
+        batch_wfs, geom, batch_mc, xs[:B], ys[:B], z_rels[:B], alphas[:B],
+        relocate_dims="yza",
+    )
+    batch_wfs_xyza, q_hat_xyza, p_hat_ = point_source_centering.relocate_simple(
+        batch_wfs, geom, batch_mc, xs[:B], ys[:B], z_rels[:B], alphas[:B],
+        relocate_dims="xyza",
+    )
+
+# %%
+fig = plt.figure()
+pca_resid_plot(batch_wfs, name="No relocation", c="k")
+pca_resid_plot(batch_wfs_yza, name="YZa relocated", c="b", pad=3)
+pca_resid_plot(batch_wfs_xyza, name="XYZA relocated", c="g", pad=4)
+plt.semilogy()
+yt = [0.5, 0.1, 0.01]
+plt.yticks(yt, list(map(str, yt)))
+plt.legend(fancybox=False)
+plt.ylabel("PCA remaining variance (s.u.)")
+plt.xlabel("number of factors")
+plt.title("Does relocation help PCA?")
+plt.show()
+
+# %%
+
+# %% [markdown]
+# ### fit PCAs on the whole data
+
+# %%
+K = 5
+
+# %%
+ipca_orig = IncrementalPCA(n_components=K)
+ipca_yza = IncrementalPCA(n_components=K)
+ipca_xyza = IncrementalPCA(n_components=K)
+
+with h5py.File(original_h5, "r") as orig_f:
+    waveforms = orig_f["denoised_waveforms"]
+    maxchans = orig_f["max_channels"][:]
+    
+    ckw = dict(geomkind="updown")
+    if "first_channels" in orig_f:
+        ckw = dict(geomkind="firstchan", firstchans=orig_f["first_channels"][:])
+    
+    for b in trange((N + 1) // 2048, desc="fit"):
+        start = b * 2048
+        end = min(N, (b + 1) * 2048)
+
+        wfs_orig = waveforms[start:end]
+        B, _, _ = wfs_orig.shape
+        
+        wfs_yza, _, _ = point_source_centering.relocate_simple(
+            wfs_orig,
+            geom,
+            maxchans[start:end],
+            xs[start:end],
+            ys[start:end],
+            z_rels[start:end],
+            alphas[start:end],
+            channel_radius=10,
+            relocate_dims="yza",
+            **ckw,
+        )
+        wfs_xyza, _, _ = point_source_centering.relocate_simple(
+            wfs_orig,
+            geom,
+            maxchans[start:end],
+            xs[start:end],
+            ys[start:end],
+            z_rels[start:end],
+            alphas[start:end],
+            channel_radius=10,
+            relocate_dims="xyza",
+            **ckw,
+        )
+        ipca_orig.partial_fit(wfs_orig.reshape(B, -1))
+        ipca_yza.partial_fit(wfs_yza.reshape(B, -1))
+        ipca_xyza.partial_fit(wfs_xyza.reshape(B, -1))        
+
+# %%
+loadings_orig = np.empty((N, K))
+loadings_yza = np.empty((N, K))
+loadings_xyza = np.empty((N, K))
+
+with h5py.File(original_h5, "r") as orig_f:
+    waveforms = orig_f["denoised_waveforms"]
+    maxchans = orig_f["max_channels"][:]
+    
+    ckw = dict(geomkind="updown")
+    if "first_channels" in orig_f:
+        ckw = dict(geomkind="firstchan", firstchans=orig_f["first_channels"][:])
+
+    for b in trange((N + 1) // 2048, desc="project"):
+        start = b * 2048
+        end = min(N, (b + 1) * 2048)
+
+        wfs_orig = waveforms[start:end]
+        B, _, _ = wfs_orig.shape
+        wfs_yza, _, _ = point_source_centering.relocate_simple(
+            wfs_orig,
+            geom,
+            maxchans[start:end],
+            xs[start:end],
+            ys[start:end],
+            z_rels[start:end],
+            alphas[start:end],
+            channel_radius=10,
+            relocate_dims="yza",
+            **ckw,
+        )
+        wfs_xyza, _, _ = point_source_centering.relocate_simple(
+            wfs_orig,
+            geom,
+            maxchans[start:end],
+            xs[start:end],
+            ys[start:end],
+            z_rels[start:end],
+            alphas[start:end],
+            channel_radius=10,
+            relocate_dims="xyza",
+            **ckw,
+        )
+        wfs_orig = wfs_orig.reshape(end - start, -1)
+        wfs_yza = wfs_yza.reshape(end - start, -1)
+        wfs_xyza = wfs_xyza.reshape(end - start, -1)
+
+        loadings_orig[start:end] = ipca_orig.transform(wfs_orig)
+        loadings_yza[start:end] = ipca_yza.transform(wfs_yza)
+        loadings_xyza[start:end] = ipca_xyza.transform(wfs_xyza)
+
+# %%
+pcs_orig = ipca_orig.components_.reshape(K, T, C)
+pcs_yza = ipca_yza.components_.reshape(K, T, C)
+pcs_xyza = ipca_xyza.components_.reshape(K, T, C)
+
+# %%
+vis_utils.labeledmosaic(
+    [pcs_orig[:, 15:, :], pcs_yza[:, 15:, :], pcs_xyza[:, 15:, :]],
+    ["orig. PCs", "yza PCs", "xyza PCs"],
+    collabels="12345",
+    pad=1,
+)
+
+# %%
+with h5py.File(feats_h5, "r+") as feats_f:
+    feats_f.create_dataset("loadings_orig", data=loadings_orig)
+    feats_f.create_dataset("loadings_yza", data=loadings_yza)
+    feats_f.create_dataset("loadings_xyza", data=loadings_xyza)
+    feats_f.create_dataset("pcs_orig", data=pcs_orig)
+    feats_f.create_dataset("pcs_yza", data=pcs_yza)
+    feats_f.create_dataset("pcs_xyza", data=pcs_xyza)
+
+# %%
+with h5py.File(original_h5, "r") as original_f:
+    N, T, C = original_f["denoised_waveforms"].shape
+
+# %%
+with h5py.File(feats_h5, "r") as feats_f:
+    for k in feats_f.keys():
+        print(k, feats_f[k].dtype, feats_f[k].shape)
+    alphas = feats_f["alpha"][:]
+    loadings_orig = feats_f["loadings_orig"][:]
+    loadings_yza = feats_f["loadings_yza"][:]
+    loadings_xyza = feats_f["loadings_xyza"][:]
+    maxptp = feats_f["maxptp"][:]
+    pcs_orig = feats_f["pcs_orig"][:]
+    pcs_xyza = feats_f["pcs_xyza"][:]
+    pcs_yza = feats_f["pcs_yza"][:]
+    times = feats_f["times"][:]
+    xs = feats_f["x"][:]
+    ys = feats_f["y"][:]
+    z_abss = feats_f["z_abs"][:]
+    z_disp = feats_f["z_disp"][:]
+    z_reg = feats_f["z_reg"][:]
+    z_rels = feats_f["z_rel"][:]
+
+# %% [markdown]
+# ## invariance / clustering
+
+# %% tags=[]
+good = np.flatnonzero(ys > 0.05)
+
+# this handles a bug now fixed above, remove after next PCA fit
+# good = good[good < 2048 * (N // 2048)]
+good = np.setdiff1d(good, np.flatnonzero(loadings_orig[:, 0] == 0))
+good = np.setdiff1d(good, np.flatnonzero(loadings_yza[:, 0] == 0))
+good = np.setdiff1d(good, np.flatnonzero(loadings_xyza[:, 0] == 0))
+
+# this handles some bug with duplicated spikes, see the bug tracker
+with h5py.File(original_h5, "r") as orig_f:
+    _, unique_inds = np.unique(orig_f["spike_index"][:, 0], return_index=True)
+good = np.intersect1d(good, unique_inds)
+
+feats_orig = np.ascontiguousarray(np.c_[xs, ys, z_reg, alphas, loadings_orig[:, :2]][good]).T
+feats_yza = np.ascontiguousarray(np.c_[xs, ys, z_reg, alphas, loadings_yza[:, :2]][good]).T
+feats_xyza = np.ascontiguousarray(np.c_[xs, ys, z_reg, alphas, loadings_xyza[:, :2]][good]).T
+
+# %%
+shuffle = rg().permutation(len(good))
+invshuf = np.empty_like(shuffle)
+for i, j in enumerate(shuffle):
+    invshuf[j] = i
+
+# %%
+with timer("isosplit"):
+    print("Running clustering on original ...")
+    labels_orig = isosplit(feats_orig[:, shuffle], K_init=512)
+    labels_orig = labels_orig[invshuf]
+    print("Got", labels_orig.max() + 1, "clusters")
+    labels_orig_full = np.full(N, -1, dtype=np.int32)
+    labels_orig_full[good] = labels_orig
+
+# %%
+with timer("isosplit"):
+    print("Running clustering on yza ...")
+    labels_yza = isosplit(feats_yza[:, shuffle], K_init=512)
+    labels_yza = labels_yza[invshuf]
+    print("Got", labels_yza.max() + 1, "clusters")
+    labels_yza_full = np.full(N, -1, dtype=np.int32)
+    labels_yza_full[good] = labels_yza
+
+# %%
+with timer("isosplit"):
+    print("Running clustering on xyza ...")
+    labels_xyza = isosplit(feats_xyza[:, shuffle], K_init=512)
+    labels_xyza = labels_xyza[invshuf]
+    print("Got", labels_xyza.max() + 1, "clusters")
+    labels_xyza_full = np.full(N, -1, dtype=np.int32)
+    labels_xyza_full[good] = labels_xyza
+
+# %%
+with h5py.File(feats_h5, "r+") as feats_f:
+    good_mask = np.zeros(N, dtype=bool)
+    good_mask[good] = 1
+    feats_f.create_dataset("good_mask", data=good_mask)
+    feats_f.create_dataset("labels_orig", data=labels_orig_full)
+    feats_f.create_dataset("labels_yza", data=labels_yza_full)
+    feats_f.create_dataset("labels_xyza", data=labels_xyza_full)
+
+# %%
+with h5py.File(feats_h5, "r") as feats_f:
+    good_mask = feats_f["good_mask"][:]
+    labels_orig = feats_f["labels_orig"][:]
+    labels_yza = feats_f["labels_yza"][:]
+    labels_xyza = feats_f["labels_xyza"][:]
+
+# %%
+a = np.linspace(0, 5, 10)
+b = 5 * a
+# a[5] = -1
+statistics.gcs(a, b) 
+
+# %%
+statistics.spearmanr(a, b)
 
 # %%
