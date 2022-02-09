eab5b1b05ffa1796a04a4f72cf17da403d282385
Charlie Windolf <cwindolf95@gmail.com>
2022-02-09 15:25:50 -0500

    Linear autoencoder thing

--- spike_psvae/featurize.py
+++ spike_psvae/featurize.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy import sparse
+from sklearn.decomposition import PCA
+from tqdm.auto import trange
 
 from . import localization, point_source_centering
 from . import up_down
@@ -15,7 +17,7 @@ def isotonic_ptp(ptps, central=False):
                 ].ravel()
                 for ptp in ptps
             ],
-            dtype=ptps.dtype
+            dtype=ptps.dtype,
         )
     else:
         return np.array(
@@ -26,7 +28,7 @@ def isotonic_ptp(ptps, central=False):
                 ].ravel()
                 for ptp in ptps
             ],
-            dtype=ptps.dtype
+            dtype=ptps.dtype,
         )
 
 
@@ -82,3 +84,71 @@ def featurize(
     recons = (u @ np.diag(s) @ vh).reshape((n, t, c)) + means
     recons_inv = recons * (p / q).numpy()[:, None, :]
     return features, recons_inv
+
+
+def pca_reload(
+    original_waveforms,
+    relocated_waveforms,
+    orig_ptps,
+    standard_ptps,
+    rank=10,
+    B_updates=0,
+):
+    N, T, C = original_waveforms.shape
+    assert relocated_waveforms.shape == (N, T, C)
+    assert orig_ptps.shape == standard_ptps.shape == (N, C)
+
+    destandardization = (orig_ptps / standard_ptps)[:, None, :]
+
+    # fit PCA in relocated space
+    reloc_pca = PCA(rank).fit(relocated_waveforms.reshape(N, T * C))
+    pca_basis = reloc_pca.components_.reshape(rank, T, C)
+
+    # rank 0 model
+    relocated_mean = reloc_pca.mean_.reshape(T, C)
+    unrelocated_means = relocated_mean[None, :, :] * destandardization
+    decentered_original_waveforms = original_waveforms - unrelocated_means
+
+    # re-compute the loadings to minimize loss in original space
+    reloadings = np.zeros((N, rank))
+    err = 0.0
+    for n in trange(N):
+        A = (
+            (destandardization[n, None, :, :] * pca_basis)
+            .reshape(rank, T * C)
+            .T
+        )
+        b = decentered_original_waveforms[n].reshape(T * C)
+        x, resid, *_ = np.linalg.lstsq(A, b, rcond=None)
+        reloadings[n] = x
+        err += resid
+    err = err / (N * T * C)
+
+    for _ in trange(B_updates, desc="B updates"):
+        # update B
+        # flat view
+        B = pca_basis.reshape(rank, T * C)
+        W = decentered_original_waveforms.reshape(N, T * C)
+        for c in range(C):
+            A = destandardization[:, 0, c, None] * reloadings
+            for t in range(T):
+                i = t * C + c
+                res, *_ = np.linalg.lstsq(A, W[:, i], rcond=None)
+                B[:, i] = res
+
+        # re-update reloadings
+        reloadings = np.zeros((N, rank))
+        err = 0.0
+        for n in trange(N):
+            A = (
+                (destandardization[n, None, :, :] * pca_basis)
+                .reshape(rank, T * C)
+                .T
+            )
+            b = decentered_original_waveforms[n].reshape(T * C)
+            x, resid, *_ = np.linalg.lstsq(A, b, rcond=None)
+            reloadings[n] = x
+            err += resid
+        err = err / (N * T * C)
+
+    return reloadings, err
