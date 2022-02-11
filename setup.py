9881e08706f9c295dba25311fd22366302443870
Charlie Windolf <cwindolf95@gmail.com>
2022-02-11 16:04:06 -0500

    Do not build c extensions unless you can

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup
-from Cython.Build import cythonize
+
+try:
+    from Cython.Build import cythonize
+    ext_modules = cythonize("spike_psvae/jisotonic5.pyx")
+except ImportError:
+    ext_modules = None
 
 
 setup(
     name="spike_psvae",
     version="0.1",
     packages=["spike_psvae"],
-    ext_modules=cythonize("spike_psvae/jisotonic5.pyx")
+    ext_modules=ext_modules,
 )
