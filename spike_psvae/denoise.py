d0af6628060f504feb304a2dab3043f88726c01d
Charlie Windolf <cwindolf95@gmail.com>
2022-02-13 16:34:32 -0500

    Remove weird temporal pca

--- spike_psvae/denoise.py
+++ spike_psvae/denoise.py
@@ -33,43 +33,34 @@ class SingleChanDenoiser(nn.Module):
         self.load_state_dict(checkpoint)
 
 
-def fit_temporal_pca(
-    waveforms, n_train=10_000, n_temporal_wfs=3, pca_rank=3, seed=0
-):
-    # extract training set
-    rg = np.random.default_rng(seed)
-    train_ix = rg.choice(len(waveforms), replace=False, size=n_train)
-    train_ix.sort()
+def temporal_align(waveforms, maxchans=None, offset=42):
+    N, T, C = waveforms.shape
+    if maxchans is None:
+        maxchans = waveforms.ptp(1).argmax(1)
+    offsets = waveforms[np.arange(N), :, maxchans].argmin(1)
+    rolls = offset - offsets
+    out = np.empty_like(waveforms)
+    pads = [(0, 0), (0, 0)]
+    for i, roll in enumerate(rolls):
+        if roll > 0:
+            pads[0] = (roll, 0)
+            start, end = 0, T
+        elif roll < 0:
+            pads[0] = (0, -roll)
+            start, end = -roll, T - roll
+        else:
+            out[i] = waveforms[i]
+            continue
 
-    # get temporal components
-    u, s, vh = la.svd(waveforms[train_ix], full_matrices=False)
+        pwf = np.pad(waveforms[i], pads, mode="linear_ramp")
+        out[i] = pwf[start:end, :]
 
-    # fit PCAs
-    pca_temporal = [
-        PCA(pca_rank).fit(u[:, :, k]) for k in range(n_temporal_wfs)
-    ]
-
-    return pca_temporal
-
-
-def apply_temporal_pca(pca_temporal, waveforms):
-    n, t, c = waveforms.shape
-    n_temporal_wfs = len(pca_temporal)
-    u, s, vh = la.svd(waveforms, full_matrices=False)
-    u = np.stack(
-        [
-            pca.inverse_transform(pca.transform(u[:, :, k]))
-            for k, pca in enumerate(pca_temporal)
-        ],
-        axis=-1,
-    )
-    return u @ (s[:, :n_temporal_wfs, None] * vh[:, :n_temporal_wfs])
+    return out, rolls
 
 
-def enforce_decrease(waveform):
+def enforce_decrease(waveform, in_place=False):
     n_chan = waveform.shape[1]
-    wf = waveform.copy()
-
+    wf = waveform if in_place else waveform.copy()
     max_chan = wf.ptp(0).argmax()
 
     max_chan_even = max_chan - max_chan % 2
@@ -103,4 +94,5 @@ def enforce_decrease(waveform):
                 * wf[:, max_chan_odd + i].ptp()
                 / wf[:, max_chan_odd + i + 2].ptp()
             )
+
     return wf
