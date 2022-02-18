c72a90db3a7ec689c47862106fc8008c4fd8e322
Charlie Windolf <cwindolf95@gmail.com>
2022-02-18 16:35:43 -0500

    Big refactor probably a lot of bugs coming down the pipe

--- spike_psvae/localization.py
+++ spike_psvae/localization.py
@@ -1,63 +1,27 @@
-from itertools import repeat
 from joblib import Parallel, delayed
 import numpy as np
-from scipy.optimize import least_squares, minimize
-from tqdm.auto import trange, tqdm
-import scipy
-from .waveform_utils import get_local_geom, get_local_chans
+from scipy.optimize import minimize
+from tqdm.auto import tqdm
+from .waveform_utils import get_local_geom
 
 # (x_low, y_low, z_low, alpha_low), (x_high, y_high, z_high, alpha_high)
-BOUNDS_NP1 = (-100, 1e-4, -100, 0), (132, 250, 100, 20000)
-BOUNDS_NP2 = (-100, 1e-4, -100, 0), (132, 250, 100, 20000)
-BOUNDS = {20: BOUNDS_NP1, 15: BOUNDS_NP2}
+# BOUNDS_NP1 = (-100, 1e-4, -100, 0), (132, 250, 100, 20000)
+# BOUNDS_NP2 = (-100, 1e-4, -100, 0), (132, 250, 100, 20000)
+BOUNDS_NP1 = [(-100, 132), (1e-4, 250), (-100, 100)]
+BOUNDS = {20: BOUNDS_NP1, 15: BOUNDS_NP1}
 
 # how to initialize y, alpha?
 Y0, ALPHA0 = 20.0, 1000.0
 
 
-def check_shapes(
-    waveforms, maxchans, channel_radius, geom, firstchans, geomkind
-):
+def check_shapes(waveforms, maxchans, geom, firstchans):
     N, T, C = waveforms.shape
     C_, d = geom.shape
     assert d == 2
-
-    if C == 2 * channel_radius:
-        assert geomkind in ("updown", "firstchan")
-        print(f"Waveforms are already trimmed to {C} channels.")
-        if geomkind == "updown" and maxchans is None:
-            # we will need maxchans later to determine local geometries
-            raise ValueError(
-                "maxchans can't be None when geomkind==updown and "
-                "waveform channels < geom channels"
-            )
-        if geomkind == "firstchan" and firstchans is None:
-            raise ValueError(
-                "firstchans can't be None when geomkind==firstchan"
-            )
-    elif C == 2 * channel_radius + 2:
-        assert geomkind in ("standard", "firstchanstandard")
-        print(f"Waveforms are already trimmed to {C} channels.")
-        if maxchans is None:
-            # we will need maxchans later to determine local geometries
-            raise ValueError(
-                "maxchans can't be None when waveform channels < geom channels"
-            )
-        if geomkind == "firstchanstandard" and firstchans is None:
-            raise ValueError(
-                "firstchans can't be None when geomkind==firstchanstandard"
-            )
-    elif C == C_:
-        print(
-            f"Waveforms are on all {C} channels. "
-            f"Trimming with radius {channel_radius}."
-        )
-    else:
-        raise ValueError(
-            f"Not sure what to do with waveforms.shape={waveforms.shape} "
-            f"and channel_radius={channel_radius} in geomkind {geomkind}"
-        )
-
+    assert C <= C_
+    assert (firstchans + C <= C_).all()
+    assert (firstchans <= maxchans).all()
+    assert (maxchans < firstchans + C).all()
     return N, T, C
 
 
@@ -71,12 +35,9 @@ def ptp_at(x, y, z, alpha, local_geom):
 
 def localize_ptp(
     ptp,
+    firstchan,
     maxchan,
     geom,
-    jac=False,
-    logbarrier=True,
-    firstchan=None,
-    geomkind="updown",
 ):
     """Find the localization result for a single ptp vector
 
@@ -90,19 +51,13 @@ def localize_ptp(
     -------
     x, y, z_rel, z_abs, alpha
     """
-    if logbarrier:
-        assert not jac
-    # print(scipy.__version__)
-    channel_radius = ptp.shape[0] // 2 - ("standard" in geomkind)
-    # local_geom is 2*channel_radius, 2
+    n_channels = ptp.shape[1]
     local_geom, z_maxchan = get_local_geom(
         geom,
+        firstchan,
         maxchan,
-        channel_radius,
-        ptp,
+        n_channels,
         return_z_maxchan=True,
-        firstchan=firstchan,
-        geomkind=geomkind,
     )
 
     # initialize x, z with CoM
@@ -112,68 +67,43 @@ def localize_ptp(
     xcom, zcom = (ptp_p[:, None] * local_geom).sum(axis=0)
     maxptp = ptp.max()
 
-    if logbarrier:
-        def residual(loc):
-            x, y, z, alpha = loc
-            phat = ptp_at(x, y, z, alpha, local_geom)
-            
-            qhat = ptp_at(x, y, z, ALPHA0, local_geom)
-            logpenalty = np.log(1 + np.log(1 + maxptp * y) / 50)
-            sse = np.square(ptp - ptp_at(x, y, z, alpha, local_geom)).sum()
-            return sse - logpenalty # / len(ptp)
-    else:
-        def residual(loc):
-            return ptp - ptp_at(*loc, local_geom)
+    def ptp_at(x, y, z, alpha):
+        return alpha / np.sqrt(
+            np.square(x - local_geom[:, 0])
+            + np.square(z - local_geom[:, 1])
+            + np.square(y)
+        )
 
-    jacobian = "2-point"
-    if jac and not logbarrier:
+    def mse(loc):
+        x, y, z = loc
+        q = ptp_at(x, y, z, 1.0)
+        alpha = (q * ptp / maxptp).sum() / (q * q).sum()
+        return (
+            np.square(ptp / maxptp - ptp_at(x, y, z, alpha)).mean()
+            - np.log1p(10.0 * y) / 10000.0
+        )
 
-        def jacobian(loc):
-            x, y, z, alpha = loc
-            dxz = np.array(((x, z),)) - local_geom
-            sq_dxz = np.square(dxz)
-            sqdists = (y ** 2 + sq_dxz).sum(axis=1)
-            d12 = np.sqrt(sqdists)
-            inv_d32 = 1.0 / (sqdists * d12)
-            ddx = alpha * dxz[:, 0] * inv_d32
-            ddy = alpha * y * inv_d32
-            ddz = alpha * dxz[:, 1] * inv_d32
-            dda = -1.0 / d12
-            return np.stack((ddx, ddy, ddz, dda), axis=1)
-
-    # if logbarrier:
-    #     result = minimize(
-    #         residual,
-    #         # jac=jacobian,
-    #         x0=[xcom, Y0, zcom, ALPHA0],
-    #         bounds=list(zip(*BOUNDS[int(geom[2, 1] - geom[0, 1])])),
-    #         # options=dict(disp=True)
-    #     )
-        
-    result = least_squares(
-        residual,
-        # jac=jacobian,
-        x0=[xcom, Y0, zcom, ALPHA0],
-        bounds=BOUNDS[int(geom[2, 1] - geom[0, 1])],
+    result = minimize(
+        mse,
+        x0=[xcom, Y0, zcom],
+        bounds=[(-100, 132), (1e-4, 250), (-100, 100)],
     )
 
-    # convert to absolute positions
-    x, y, z_rel, alpha = result.x
-    z_abs = z_rel + z_maxchan
-
-    return x, y, z_rel, z_abs, alpha
+    # print(result)
+    bx, by, bz_rel = result.x
+    q = ptp_at(bx, by, bz_rel, 1.0)
+    balpha = (ptp * q).sum() / np.square(q).sum()
+    return bx, by, bz_rel, geom[maxchan, 1] + bz_rel, balpha
 
 
 def localize_ptps(
     ptps,
     geom,
+    firstchans,
     maxchans,
-    channel_radius=10,
     n_workers=None,
     jac=False,
     logbarrier=True,
-    firstchans=None,
-    geomkind="updown",
     _not_helper=True,
 ):
     """Localize a bunch of waveforms
@@ -193,18 +123,14 @@ def localize_ptps(
         N, _, C = check_shapes(
             ptps[:, None, :],
             maxchans,
-            channel_radius,
             geom,
             firstchans,
-            geomkind,
         )
     else:
         N, C = ptps.shape
 
-    # I have them stored as floats and keep forgetting to int them.
     maxchans = maxchans.astype(int)
-    if firstchans is None:
-        firstchans = repeat(None)
+    firstchans = firstchans.astype(int)
 
     # handle pbars
     xqdm = tqdm if _not_helper else lambda a, total, desc: a
@@ -220,11 +146,10 @@ def localize_ptps(
             pool(
                 delayed(localize_ptp)(
                     ptp,
+                    firstchan,
                     maxchan,
                     geom,
                     jac=jac,
-                    firstchan=firstchan,
-                    geomkind=geomkind,
                     logbarrier=logbarrier,
                 )
                 for ptp, maxchan, firstchan in xqdm(
@@ -244,13 +169,11 @@ def localize_ptps(
 def localize_waveforms(
     waveforms,
     geom,
-    maxchans=None,
-    channel_radius=10,
+    firstchans,
+    maxchans,
     n_workers=1,
     jac=False,
     logbarrier=True,
-    firstchans=None,
-    geomkind="updown",
     _not_helper=True,
 ):
     """Localize a bunch of waveforms
@@ -267,52 +190,17 @@ def localize_waveforms(
         assert not jac
 
     if _not_helper:
-        N, T, C = check_shapes(
-            waveforms, maxchans, channel_radius, geom, firstchans, geomkind
-        )
+        N, T, C = check_shapes(waveforms, maxchans, geom, firstchans)
     else:
         N, T, C = waveforms.shape
 
-    # I have them stored as floats and keep forgetting to int them.
-    if maxchans is not None:
-        maxchans = maxchans.astype(int)
+    maxchans = maxchans.astype(int)
 
     # handle pbars
-    xrange = trange if _not_helper else lambda a, desc: range(a)
     xqdm = tqdm if _not_helper else lambda a, total, desc: a
 
-    # -- get N x local_neighborhood_size array of PTPs
-    if C in (2 * channel_radius, 2 * channel_radius + 2):
-        ptps = waveforms.ptp(axis=1)
-    else:
-        # we need maxchans to extract the local waveform
-        ptps_full = waveforms.ptp(axis=1)
-        if maxchans is None:
-            maxchans = np.argmax(ptps_full, axis=1)
-            bad = np.flatnonzero(ptps_full.ptp(1) == 0)
-            if bad:
-                raise ValueError(f"Some waveforms were all zero: {bad}.")
-
-        ptps = np.empty(
-            (N, 2 * channel_radius + 2 * ("standard" in geomkind)),
-            dtype=waveforms.dtype,
-        )
-        for n in xrange(N, desc="extracting channels"):
-            low, high = get_local_chans(
-                geom,
-                maxchans[n],
-                channel_radius,
-                ptps_full[n],
-                firstchan=firstchans[n],
-                geomkind=geomkind,
-            )
-            ptps[n] = ptps_full[n, low:high]
-        del ptps_full
-
-    if firstchans is None:
-        firstchans = repeat(None)
-
     # -- run the least squares
+    ptps = waveforms.ptp(1)
     xs = np.empty(N)
     ys = np.empty(N)
     z_rels = np.empty(N)
@@ -323,11 +211,10 @@ def localize_waveforms(
             pool(
                 delayed(localize_ptp)(
                     ptp,
+                    firstchan,
                     maxchan,
                     geom,
                     jac=jac,
-                    firstchan=firstchan,
-                    geomkind=geomkind,
                     logbarrier=logbarrier,
                 )
                 for ptp, maxchan, firstchan in xqdm(
@@ -347,22 +234,18 @@ def localize_waveforms(
 def localize_waveforms_batched(
     waveforms,
     geom,
-    maxchans=None,
-    channel_radius=10,
+    firstchans,
+    maxchans,
     n_workers=1,
     jac=False,
     logbarrier=True,
-    firstchans=None,
-    geomkind="updown",
     batch_size=128,
 ):
     """A helper for running the above on hdf5 datasets or similar"""
     if logbarrier:
         assert not jac
 
-    N, T, C = check_shapes(
-        waveforms, maxchans, channel_radius, geom, firstchans, geomkind
-    )
+    N, T, C = check_shapes(waveforms, maxchans, geom, firstchans)
     xs = np.empty(N)
     ys = np.empty(N)
     z_rels = np.empty(N)
@@ -372,29 +255,15 @@ def localize_waveforms_batched(
     starts = list(range(0, N, batch_size))
     ends = [min(start + batch_size, N) for start in starts]
 
-    def maxchan_batch(start, end):
-        if maxchans is None:
-            return None
-        else:
-            return maxchans[start:end]
-
-    def firstchan_batch(start, end):
-        if firstchans is None:
-            return None
-        else:
-            return firstchans[start:end]
-
     with Parallel(n_workers) as pool:
         for batch_idx, (x, y, z_rel, z_abs, alpha) in enumerate(
             pool(
                 delayed(localize_waveforms)(
                     waveforms[start:end],
                     geom,
-                    maxchans=maxchan_batch(start, end),
-                    channel_radius=channel_radius,
+                    firstchans,
+                    maxchans,
                     jac=jac,
-                    geomkind=geomkind,
-                    firstchans=firstchan_batch(start, end),
                     logbarrier=logbarrier,
                     _not_helper=False,
                 )
