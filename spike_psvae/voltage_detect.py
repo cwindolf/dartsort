3c891abe51a6c31ac47924eb5291f91ea3314b22
Charlie Windolf <c.windolf@columbia.edu>
2022-02-23 16:28:14 -0500

    Speed up detection and simplify dedup, but the torch dedup is hard to beat...

--- spike_psvae/voltage_detect.py
+++ spike_psvae/voltage_detect.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from scipy.signal import argrelmin
 import torch.nn.functional as F
-from scipy.spatial import KDTree
+from scipy.ndimage import maximum_filter1d
 
 
 def detect_and_deduplicate(
@@ -10,19 +10,11 @@ def detect_and_deduplicate(
 ):
     spike_index, energy = voltage_threshold(recording, threshold)
 
-    # move to gpu
-    spike_index = torch.from_numpy(spike_index)
-    energy = torch.from_numpy(energy)
-    spike_index = spike_index.to(device)
-    energy = energy.to(device)
-
     # deduplicate
     spike_index_dedup, energy_dedup = deduplicate_gpu(
-    # spike_index_dedup, energy_dedup = deduplicate(
-        spike_index, energy, recording.shape, channel_index
+    # spike_index_dedup, energy_dedup = deduplicate_sp(
+        spike_index, energy, recording.shape, channel_index, #device=device
     )
-    spike_index_dedup = spike_index_dedup.cpu().numpy()
-    energy_dedup = energy_dedup.cpu().numpy()
 
     # update times wrt buffer size, remove spikes in buffer
     spike_index_dedup[:, 0] -= buffer_size
@@ -37,23 +29,11 @@ def detect_and_deduplicate(
 
 def voltage_threshold(recording, threshold, order=5):
     T, C = recording.shape
-    spike_index = []
-    energy = []
-
-    for c in range(C):
-        single_chan_rec = recording[:, c]
-        index = argrelmin(single_chan_rec, order=order)[0]
-        which = np.flatnonzero(single_chan_rec[index] < -threshold)
-        if which.size:
-            index = index[which]
-            spike_index.append(np.c_[index, np.full(len(index), c)])
-            energy.append(np.abs(single_chan_rec[index]))
-            
-    spike_index = np.concatenate(spike_index, axis=0)
-    energy = np.concatenate(energy, axis=0)
-
-
-    return spike_index, energy
+    ts, mcs = argrelmin(recording, axis=0, order=order)
+    spike_index = np.c_[ts, mcs]
+    energy = recording[ts, mcs]
+    which = energy < -threshold
+    return spike_index[which], np.abs(energy[which])
 
 
 def deduplicate(spike_index, energy, channel_index, max_window=5.):
@@ -88,38 +68,69 @@ def deduplicate(spike_index, energy, channel_index, max_window=5.):
 
 @torch.no_grad()
 def deduplicate_gpu(
-    spike_index_torch,
-    energy_torch,
+    spike_index,
+    energy,
     recording_shape,
     channel_index,
     max_window=5,
 ):
-    device = spike_index_torch.device
-
+    spike_index_torch = torch.tensor(spike_index)
+    energy_torch = torch.tensor(energy)
+    times = spike_index_torch[:, 0]
+    chans = spike_index_torch[:, 1]
+    
     # initialize energy train
     energy_train = torch.zeros(
-        recording_shape, dtype=energy_torch.dtype, device=device
+        recording_shape, dtype=energy_torch.dtype,
     )
-    energy_train[
-        spike_index_torch[:, 0], spike_index_torch[:, 1]
-    ] = energy_torch
+    energy_train[times, chans] = energy_torch
 
     # get temporal max
-    maxpool = torch.nn.MaxPool2d(
+    max_energy = F.max_pool2d(
+        energy_train[None, None], 
         kernel_size=[max_window * 2 + 1, 1],
         stride=1,
         padding=[max_window, 0],
-    )
-    max_energy = maxpool(energy_train[None, None])[0, 0]
-    
+    )[0, 0]
+
     # get spatial max
     max_energy = F.pad(max_energy, (0, 1))
     max_energy = torch.max(max_energy[:, channel_index], 2)[0] - 1e-8
 
     # deduplicated spikes: temporal and spatial local max
-    spike_index_dedup = torch.nonzero(
-        (energy_train >= max_energy) & (energy_train > 0)
+    which = torch.nonzero(energy_torch >= max_energy[times, chans]).cpu().numpy()[:, 0]
+    energy_dedup = energy[which]
+    spike_index_dedup = spike_index[which]
+
+    return spike_index_dedup, energy_dedup
+
+
+def deduplicate_sp(
+    spike_index,
+    energy,
+    recording_shape,
+    channel_index,
+    max_window=5,
+):
+    times = spike_index[:, 0]
+    chans = spike_index[:, 1]
+    
+    # initialize energy train
+    energy_train = np.zeros(
+        recording_shape, dtype=energy.dtype,
     )
-    energy_dedup = energy_train[spike_index_dedup]
+    energy_train[times, chans] = energy
+
+    # get temporal max
+    max_energy = maximum_filter1d(energy_train, 2 * max_window + 1, mode="constant", axis=0)
+
+    # get spatial max
+    max_energy = np.pad(max_energy, [(0, 0), (0, 1)])
+    max_energy = np.max(max_energy[:, channel_index], axis=2)
+
+    # deduplicated spikes: temporal and spatial local max
+    which = np.flatnonzero(energy >= max_energy[times, chans] - 1e-8)
+    energy_dedup = energy[which]
+    spike_index_dedup = spike_index[which]
 
     return spike_index_dedup, energy_dedup
