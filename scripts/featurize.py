7b70156969592f857d36219eff488a64c9524efa
Charlie Windolf <cwindolf95@gmail.com>
2022-03-03 10:42:41 -0500

    Add linear autoencoder sklearn style with easier API

--- scripts/featurize.py
+++ scripts/featurize.py
@@ -7,7 +7,7 @@ import argparse
 import numpy as np
 import h5py
 import time
-from spike_psvae import featurize
+from spike_psvae.linear_ae import LinearRelocAE
 
 
 ap = argparse.ArgumentParser()
@@ -16,7 +16,7 @@ ap.add_argument("subtracted_h5")
 ap.add_argument("locs_npz")
 ap.add_argument("output_npz")
 ap.add_argument("--rank", type=int, default=10)
-ap.add_argument("--n_jobs", type=int, default=1)
+# ap.add_argument("--n_jobs", type=int, default=1)
 
 args = ap.parse_args()
 
@@ -44,22 +44,14 @@ with timer("featurize"):
     with h5py.File(args.subtracted_h5, "r") as h5:
         with np.load(args.locs_npz) as locs_f:
             waveforms = h5["cleaned_waveforms"]
-            maxchans = h5["cleaned_max_channels"][:]
-            firstchans = h5["cleaned_first_channels"][:]
+            firstchans = h5["first_channels"][:]
             geom = h5["geom"][:]
             x, y, z_rel, z_abs, alpha = locs_f["locs"].T
 
-            features, errors = featurize.relocated_ae_batched(
-                waveforms,
-                firstchans,
-                maxchans,
-                geom,
-                x,
-                y,
-                z_rel,
-                alpha,
-                rank=args.rank,
-                n_jobs=args.n_jobs,
+            ae = LinearRelocAE(args.rank, geom)
+            ae.fit(waveforms, x, y, z_abs, alpha, firstchans)
+            features, errors = ae.transform(
+                waveforms, x, y, z_abs, alpha, firstchans, return_error=True
             )
 
 with timer("save"):
