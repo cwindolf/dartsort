5fea4cd508669bceb4377292fa16b6a2c331d53e
Charlie Windolf <c.windolf@columbia.edu>
2022-03-11 16:50:53 -0500

    Helper for making files to upload

---
+++ scripts/upload_feats.py
@@ -0,0 +1,41 @@
+import argparse
+import h5py
+import numpy as np
+from pathlib import Path
+
+from spike_psvae import waveform_utils
+
+
+ap = argparse.ArgumentParser()
+
+ap.add_argument("sub_h5")
+ap.add_argument("locs_npz")
+ap.add_argument("feats_npz")
+ap.add_argument("out_folder")
+
+args = ap.parse_args()
+
+out_folder = Path(args.out_folder)
+
+
+sub = h5py.File(args.sub_h5, "r")
+locs = np.load(args.locs_npz)
+feats = np.load(args.feats_npz)
+ls = locs["locs"]
+
+rwfs, firstchans_std, _ = waveform_utils.relativize_waveforms_np1(
+    sub["cleaned_waveforms"][:], sub["first_channels"][:], sub["geom"][:], sub["spike_index"][:, 1], feat_chans=20
+)
+
+np.save(out_folder / "ae_features.npy", feats["features"])
+np.save(
+    out_folder / "localization_results.npy",
+    np.c_[ls[:, 0], ls[:, 3], ls[:, 1], ls[:, 4], locs["maxptp"], firstchans_std, sub["spike_index"][:, 1]],
+)
+np.save(out_folder / "wfs.npy", rwfs)
+np.save(out_folder / "z_reg.npy", locs["z_reg"])
+np.save(out_folder / "np1_channel_map.npy", sub["geom"][:])
+np.save(out_folder / "ptps.npy", rwfs.ptp(1))
+np.save(out_folder / "reconstruction_errors.npy", feats["errors"])
+np.save(out_folder / "spike_index.npy", sub["spike_index"][:])
+
