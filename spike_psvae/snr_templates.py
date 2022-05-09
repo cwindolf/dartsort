45aa91b96e2a7ad2f7611e7cb5193b9d07d560bf
Charlie Windolf <cwindolf95@gmail.com>
2022-05-09 10:32:22 -0400

    Rewrite with no SI
    Former-commit-id: 39fcb583198658f05ce568907a0820b0904c176c

--- spike_psvae/snr_templates.py
+++ spike_psvae/snr_templates.py
@@ -4,7 +4,6 @@ from tempfile import TemporaryDirectory
 import spikeinterface.full as si
 from tqdm.auto import tqdm
 from sklearn.decomposition import PCA
-import gc
 
 from . import denoise
 
@@ -82,124 +81,73 @@ def get_templates(
         geom, full_channel_index
     )
 
-    # -- triaged spike train
-    spike_train = spike_train[spike_train[:, 1] >= 0]
+    # -- main loop to make templates
     units = np.unique(spike_train[:, 1])
-
-    # -- get waveform extractor
-    # this will sample random waveforms from the raw/residual for us
-    with TemporaryDirectory(prefix="si_deconv") as tempdir:
-        if cache_dir is None:
-            cache_dir = tempdir
-        cache_dir = Path(cache_dir)
-
-        print("Load raw waveforms")
-        raw_we = get_waveform_extractor(
+    units = units[units >= 0]
+    for unit in tqdm(units, desc="Denoised templates"):
+        # get raw template
+        raw_wfs = get_waveforms(
+            unit,
+            spike_train,
             raw_binary_file,
-            spike_train,
-            geom,
-            cache_dir / "raw",
+            len(geom),
             max_spikes_per_unit=max_spikes_per_unit,
-            n_jobs=n_jobs,
-            spike_length_samples=spike_length_samples,
             trough_offset=trough_offset,
-            sampling_frequency=sampling_frequency,
+            spike_length_samples=spike_length_samples,
         )
-        print("Load residual waveforms")
-        res_we = get_waveform_extractor(
+        raw_maxchans = raw_wfs.ptp(1).argmax(1)
+        denoise.enforce_decrease_shells(
+            raw_wfs,
+            raw_maxchans,
+            radial_parents,
+            in_place=True,
+        )
+        raw_template = reducer(raw_wfs, axis=0)
+        raw_ptp = raw_template.ptp(0).max()
+        snr = raw_ptp * np.sqrt(len(raw_wfs))
+        snrs[unit] = snr
+
+        if return_raw_cleaned:
+            raw_templates[unit] = raw_template
+
+        if snr > snr_threshold:
+            templates[unit] = raw_template
+            continue
+
+        # load cleaned waveforms
+        cleaned_wfs = get_waveforms(
+            unit,
+            spike_train,
             residual_binary_file,
-            spike_train,
-            geom,
-            cache_dir / "res",
+            len(geom),
+            subtracted_waveforms=subtracted_waveforms,
+            maxchans=subtracted_max_channels,
+            channel_index=extract_channel_index,
             max_spikes_per_unit=max_spikes_per_unit,
-            n_jobs=n_jobs,
-            spike_length_samples=spike_length_samples,
             trough_offset=trough_offset,
-            sampling_frequency=sampling_frequency,
+            spike_length_samples=spike_length_samples,
         )
 
-        # create cleaned waveforms and apply tpca if requested
-        tpca = None
         if do_tpca:
-            cleaned_waveforms = {
-                unit: pca_on_axis(
-                    get_unit_waveforms(
-                        unit,
-                        spike_train,
-                        res_we,
-                        subtracted_waveforms,
-                        subtracted_max_channels,
-                        extract_channel_index,
-                    ),
-                    axis=1,
-                    rank=tpca_rank,
-                )
-                for unit in tqdm(units, desc="tpca cleaned wfs")
-            }
-        print("done", flush=True)
+            cleaned_wfs = pca_on_axis(cleaned_wfs, axis=1)
 
-        # -- main loop to make templates
-        for unit in tqdm(units, desc="Denoised templates"):
-            # get raw template
-            raw_wfs = get_unit_waveforms(unit, spike_train, raw_we)
-            raw_maxchans = raw_wfs.ptp(1).argmax(1)
-            denoise.enforce_decrease_shells(
-                raw_wfs,
-                raw_maxchans,
-                radial_parents,
-                in_place=True,
-            )
-            raw_template = reducer(raw_wfs, axis=0)
-            raw_ptp = raw_template.ptp(0).max()
-            snr = raw_ptp * np.sqrt(len(raw_wfs))
-            snrs[unit] = snr
+        # enforce decrease for both, using raw maxchan
+        denoise.enforce_decrease_shells(
+            cleaned_wfs,
+            raw_maxchans,
+            radial_parents,
+            in_place=True,
+        )
+        cleaned_template = reducer(cleaned_wfs, axis=0)
 
-            if return_raw_cleaned:
-                raw_templates[unit] = raw_template
+        if return_raw_cleaned:
+            cleaned_templates[unit] = cleaned_template
 
-            if snr > snr_threshold:
-                templates[unit] = raw_template
-                continue
-
-            # load cleaned waveforms
-            if do_tpca:
-                cleaned_wfs = cleaned_waveforms[unit]
-            else:
-                cleaned_wfs = get_unit_waveforms(
-                    unit,
-                    spike_train,
-                    res_we,
-                    subtracted_waveforms,
-                    subtracted_max_channels,
-                    extract_channel_index,
-                )
-
-            # enforce decrease for both, using raw maxchan
-            denoise.enforce_decrease_shells(
-                cleaned_wfs,
-                raw_maxchans,
-                radial_parents,
-                in_place=True,
-            )
-
-            # apply TPCA to cleaned ones
-            if tpca is not None:
-                cleaned_wfs = tpca.fit_transform(
-                    cleaned_wfs.reshape(len(cleaned_wfs), -1)
-                )
-                cleaned_wfs = tpca.inverse_transform(cleaned_wfs).reshape(
-                    raw_wfs.shape
-                )
-            cleaned_template = reducer(cleaned_wfs, axis=0)
-
-            if return_raw_cleaned:
-                cleaned_templates[unit] = cleaned_template
-
-            # SNR-weighted combination to create the template
-            lerp = snr / snr_threshold
-            templates[unit] = (lerp) * raw_template + (
-                1 - lerp
-            ) * cleaned_template
+        # SNR-weighted combination to create the template
+        lerp = snr / snr_threshold
+        templates[unit] = (lerp) * raw_template + (
+            1 - lerp
+        ) * cleaned_template
 
     if return_raw_cleaned:
         return templates, snrs, raw_templates, cleaned_templates
@@ -220,111 +168,149 @@ def pca_on_axis(X, axis=-1, rank=8):
     return X
 
 
-def get_unit_waveforms(
+def get_waveforms(
     unit,
     spike_train,
-    waveform_extractor,
+    binary_file,
+    n_channels,
     subtracted_waveforms=None,
     maxchans=None,
     channel_index=None,
+    max_spikes_per_unit=500,
+    random_seed=None,
+    trough_offset=42,
+    spike_length_samples=121,
 ):
-    """Handle loading raw vs. collision-cleaned waveforms"""
-    waveforms, indices = waveform_extractor.get_waveforms(
-        unit, with_index=True
+    # choose random waveforms
+    rg = np.random.default_rng(unit if random_seed is None else random_seed)
+    which = np.flatnonzero(spike_train[:, 1] == unit)
+    N = min(max_spikes_per_unit, len(which))
+    choices = rg.choice(which, replace=False, size=N)
+
+    # load from binary
+    mmap = np.memmap(binary_file, dtype=np.float32, mode="r")
+    mmap = mmap.reshape(-1, n_channels)
+    time_ix = spike_train[choices[:, 0]] + np.arange(
+        -trough_offset, spike_length_samples - trough_offset
     )
-    # spikeinterface gave us a read-only memmap
-    waveforms = waveforms.copy()
+    waveforms = mmap[time_ix]
 
+    # add in subtracted waveforms
     if subtracted_waveforms is not None:
-        # which waveforms were sampled by the waveform extractor?
-        unit_which = np.flatnonzero(spike_train[:, 1] == unit)
-        unit_selected = unit_which[indices["spike_index"]]
-
-        # add in the subtracted wfs to make collision-cleaned wfs
         waveforms[
             np.arange(waveforms.shape[0])[:, None, None],
             np.arange(waveforms.shape[1])[None, :, None],
-            channel_index[maxchans[unit_selected]][:, None, :],
-        ] += subtracted_waveforms[unit_selected]
+            channel_index[maxchans[choices]][:, None, :],
+        ] += subtracted_waveforms[choices]
 
     return waveforms
 
 
-def get_waveform_extractor(
-    binary_file,
-    spike_train,
-    geom,
-    cache_dir,
-    max_spikes_per_unit=500,
-    n_jobs=20,
-    spike_length_samples=121,
-    trough_offset=42,
-    sampling_frequency=30_000,
-    standardized_dtype=np.float32,
-):
-    # cache directory logic, delete if empty for waveform extractor
-    cache_dir = Path(cache_dir)
-    cache_dir.mkdir(exist_ok=True, parents=True)
-    wf_cache_dir = cache_dir / f"{binary_file.stem}_template_waveforms"
-    if wf_cache_dir.exists():
-        assert wf_cache_dir.is_dir()
-        if not len(list(wf_cache_dir.glob("*"))):
-            wf_cache_dir.unlink()
+# def get_unit_waveforms(
+#     unit,
+#     spike_train,
+#     waveform_extractor,
+#     subtracted_waveforms=None,
+#     maxchans=None,
+#     channel_index=None,
+# ):
+#     """Handle loading raw vs. collision-cleaned waveforms"""
+#     waveforms, indices = waveform_extractor.get_waveforms(
+#         unit, with_index=True
+#     )
+#     # spikeinterface gave us a read-only memmap
+#     waveforms = waveforms.copy()
 
-    # get a recording extractor for our destriped data
-    recording = si.read_binary(
-        binary_file,
-        num_chan=len(geom),
-        sampling_frequency=sampling_frequency,
-        dtype=standardized_dtype,
-        time_axis=0,
-        is_filtered=True,
-    )
-    recording.set_channel_locations(geom)
+#     if subtracted_waveforms is not None:
+#         # which waveforms were sampled by the waveform extractor?
+#         unit_which = np.flatnonzero(spike_train[:, 1] == unit)
+#         unit_selected = unit_which[indices["spike_index"]]
 
-    # make dumpable NpzSortingExtractor
-    sorting = si.NumpySorting.from_times_labels(
-        times_list=spike_train[spike_train[:, 1] >= 0, 0],
-        labels_list=spike_train[spike_train[:, 1] >= 0, 1],
-        sampling_frequency=sampling_frequency,
-    )
-    si.NpzSortingExtractor.write_sorting(
-        sorting, cache_dir / "npz_sorting.npz"
-    )
-    sorting = si.NpzSortingExtractor(cache_dir / "npz_sorting.npz")
+#         # add in the subtracted wfs to make collision-cleaned wfs
+#         waveforms[
+#             np.arange(waveforms.shape[0])[:, None, None],
+#             np.arange(waveforms.shape[1])[None, :, None],
+#             channel_index[maxchans[unit_selected]][:, None, :],
+#         ] += subtracted_waveforms[unit_selected]
 
-    # spikeinterface uses trough offset etc in units of ms
-    ms_before = trough_offset * 1000 / sampling_frequency
-    ms_after = (
-        (spike_length_samples - trough_offset) * 1000 / sampling_frequency
-    )
-    # check no issues with rounding
-    assert int(ms_before * sampling_frequency / 1000) == trough_offset
-    assert (
-        int(ms_after * sampling_frequency / 1000)
-        == spike_length_samples - trough_offset
-    )
+#     return waveforms
 
-    # make waveform extractor
-    we = si.extract_waveforms(
-        recording,
-        sorting,
-        wf_cache_dir,
-        # overwrite=True,
-        ms_before=ms_before,
-        ms_after=ms_after,
-        max_spikes_per_unit=max_spikes_per_unit,
-        n_jobs=n_jobs,
-        chunk_size=sampling_frequency,
-        progress_bar=True,
-        return_scaled=False,
-        load_if_exists=True,
-    )
 
-    # check assumptions on what this thing does
-    test_wf, ix = we.get_waveforms(spike_train[0, 1], with_index=True)
-    assert test_wf.shape[0] == len(ix)
-    assert test_wf.shape[1] == spike_length_samples
-    assert test_wf.shape[2] == len(geom)
+# def get_waveform_extractor(
+#     binary_file,
+#     spike_train,
+#     geom,
+#     cache_dir,
+#     max_spikes_per_unit=500,
+#     n_jobs=20,
+#     spike_length_samples=121,
+#     trough_offset=42,
+#     sampling_frequency=30_000,
+#     standardized_dtype=np.float32,
+# ):
+#     # cache directory logic, delete if empty for waveform extractor
+#     cache_dir = Path(cache_dir)
+#     cache_dir.mkdir(exist_ok=True, parents=True)
+#     wf_cache_dir = cache_dir / f"{binary_file.stem}_template_waveforms"
+#     if wf_cache_dir.exists():
+#         assert wf_cache_dir.is_dir()
+#         if not len(list(wf_cache_dir.glob("*"))):
+#             wf_cache_dir.unlink()
 
-    return we
+#     # get a recording extractor for our destriped data
+#     recording = si.read_binary(
+#         binary_file,
+#         num_chan=len(geom),
+#         sampling_frequency=sampling_frequency,
+#         dtype=standardized_dtype,
+#         time_axis=0,
+#         is_filtered=True,
+#     )
+#     recording.set_channel_locations(geom)
+
+#     # make dumpable NpzSortingExtractor
+#     sorting = si.NumpySorting.from_times_labels(
+#         times_list=spike_train[spike_train[:, 1] >= 0, 0],
+#         labels_list=spike_train[spike_train[:, 1] >= 0, 1],
+#         sampling_frequency=sampling_frequency,
+#     )
+#     si.NpzSortingExtractor.write_sorting(
+#         sorting, cache_dir / "npz_sorting.npz"
+#     )
+#     sorting = si.NpzSortingExtractor(cache_dir / "npz_sorting.npz")
+
+#     # spikeinterface uses trough offset etc in units of ms
+#     ms_before = trough_offset * 1000 / sampling_frequency
+#     ms_after = (
+#         (spike_length_samples - trough_offset) * 1000 / sampling_frequency
+#     )
+#     # check no issues with rounding
+#     assert int(ms_before * sampling_frequency / 1000) == trough_offset
+#     assert (
+#         int(ms_after * sampling_frequency / 1000)
+#         == spike_length_samples - trough_offset
+#     )
+
+#     # make waveform extractor
+#     we = si.extract_waveforms(
+#         recording,
+#         sorting,
+#         wf_cache_dir,
+#         # overwrite=True,
+#         ms_before=ms_before,
+#         ms_after=ms_after,
+#         max_spikes_per_unit=max_spikes_per_unit,
+#         n_jobs=n_jobs,
+#         chunk_size=sampling_frequency,
+#         progress_bar=True,
+#         return_scaled=False,
+#         load_if_exists=True,
+#     )
+
+#     # check assumptions on what this thing does
+#     test_wf, ix = we.get_waveforms(spike_train[0, 1], with_index=True)
+#     assert test_wf.shape[0] == len(ix)
+#     assert test_wf.shape[1] == spike_length_samples
+#     assert test_wf.shape[2] == len(geom)
+
+#     return we
