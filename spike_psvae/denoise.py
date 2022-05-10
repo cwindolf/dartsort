2dcae630d551c0acb052657fec3e09e0d734cd44
Charlie Windolf <cwindolf95@gmail.com>
2022-05-09 22:32:48 -0400

    Add fast temporal decrease
    Former-commit-id: fbef55965abd9034e804dc4c5f73e43dba073ea0

--- spike_psvae/denoise.py
+++ spike_psvae/denoise.py
@@ -10,6 +10,8 @@ from sklearn.decomposition import PCA
 from torch import nn
 from tqdm.auto import trange
 
+from .denoise_temporal_decrease import _enforce_temporal_decrease
+
 
 pretrained_path = (
     Path(__file__).parent.parent / "pretrained/single_chan_denoiser.pt"
@@ -339,6 +341,46 @@ def enforce_decrease_shells(
         (decreasing_ptps / orig_ptps)[:, None, :],
         out=waveforms if in_place else None,
     )
+
+
+def enforce_temporal_decrease(
+    waveforms,
+    left=True,
+    right=True,
+    trough_offset=42,
+    center_radius=50,
+    in_place=False,
+):
+    """Enforce monotonicity of abs values at the edges
+
+    Finds the peaks to the left and right of the trough, and
+    makes sure we decrease to either side of those.
+    """
+    N, T, C = waveforms.shape
+    waveforms = waveforms.transpose(0, 2, 1).reshape(N * C, T)
+
+    if not in_place:
+        waveforms = waveforms.copy()
+
+    if left and center_radius < trough_offset:
+        # probably not good to do this in a data-driven way
+        # because of collisions
+        # left_peaks = waveforms[:, :trough_offset].argmax(1)
+        left_peaks = np.full(trough_offset - center_radius)
+        _enforce_temporal_decrease(
+            waveforms[:, ::-1],
+            T - left_peaks,
+        )
+
+    if right and center_radius < (T - trough_offset):
+        # right_peaks = waveforms[:, trough_offset:].argmax(1)
+        right_peaks = np.full(trough_offset + center_radius)
+        _enforce_temporal_decrease(
+            waveforms,
+            right_peaks,
+        )
+
+    return waveforms
 
 
 @torch.no_grad()
---
+++ spike_psvae/denoise_temporal_decrease.pyx
@@ -0,0 +1,30 @@
+#!python
+#cython: language_level=3
+import numpy as np
+from libc.math cimport abs
+cimport cython
+
+
+ctypedef fused floating:
+   double
+   float
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def _enforce_temporal_decrease(
+    floating[:,:] temporal_traces,
+    const int[:] peak_times,
+):
+    cdef Py_ssize_t M = temporal_traces.shape[0]
+    cdef Py_ssize_t T = temporal_traces.shape[1]
+    cdef int j, p 
+    cdef floating prev, cur
+
+    for j in range(M):
+        p = peak_times[j]
+        prev = abs(temporal_traces[j, p])
+        for t in range(p, T):
+            cur = abs(temporal_traces[j, t])
+            if cur > prev and cur > 0:
+                temporal_traces[j, t] *= prev / cur
