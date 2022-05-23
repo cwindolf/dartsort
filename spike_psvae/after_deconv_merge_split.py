91dbee28c93aa31fdf85c515938e589e19d8f743
julien <jb4365@columbia.edu>
2022-05-23 11:47:14 -0400

    fix bugs after deoconv
    Former-commit-id: 42930a636d31db85f853e68f2f8e6b677e953a91

--- spike_psvae/after_deconv_merge_split.py
+++ spike_psvae/after_deconv_merge_split.py
@@ -16,7 +16,6 @@ from pathlib import Path
 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from isosplit import isocut
-import merge_split_cleaned
 
 from spike_psvae import cluster, merge_split_cleaned, cluster_viz_index, denoise, cluster_utils, triage, cluster_viz
 from spike_psvae.cluster_utils import read_waveforms, compare_two_sorters, make_sorting_from_labels_frames
