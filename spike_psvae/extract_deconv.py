0100a93affadd6f71aa869aa46f75963a490fc8b
Charlie Windolf <cwindolf95@gmail.com>
2022-09-09 09:38:20 -0400

    Support amplitude scaling in extract_deconv
    Former-commit-id: 924caf8bf3b6792c21f76c821929c0c48af67a14

--- spike_psvae/extract_deconv.py
+++ spike_psvae/extract_deconv.py
@@ -15,6 +15,7 @@ def extract_deconv(
     spike_train_up_path,
     output_directory,
     standardized_bin,
+    scalings_path=None,
     channel_index=None,
     n_channels_extract=20,
     subtraction_h5=None,
@@ -40,6 +41,12 @@ def extract_deconv(
     n_templates, spike_length_samples, n_chans = templates_up.shape
     spike_train_up = np.load(spike_train_up_path)
     n_spikes = len(spike_train_up)
+
+    if scalings_path is not None:
+        scalings = np.load(scalings_path)
+        assert scalings.shape == (n_spikes,)
+    else:
+        scalings = np.ones(n_spikes, dtype=templates_up.dtype)
 
     std_size = standardized_bin.stat().st_size
     assert not std_size % np.dtype(np.float32).itemsize
@@ -120,6 +127,7 @@ def extract_deconv(
             h5.create_dataset("channel_index", data=channel_index)
             h5.create_dataset("templates_loc", data=templates_loc)
             h5.create_dataset("spike_train_up", data=spike_train_up)
+            h5.create_dataset("scalings", data=scalings)
             h5.create_dataset("spike_index_up", data=spike_index_up)
             h5.create_dataset(
                 "first_channels",
@@ -171,6 +179,7 @@ def extract_deconv(
                 temp_dir,
                 T_samples,
                 templates_loc,
+                scalings,
             ),
             context=ctx,
         ) as pool:
@@ -232,6 +241,7 @@ def _extract_deconv_worker(start_sample):
     )
     spike_index = p.spike_index_up[which_spikes]
     spike_train = p.spike_train_up[which_spikes]
+    scalings = p.scalings[which_spikes]
 
     # load raw recording with padding
     at_start = start_sample == 0
@@ -263,9 +273,9 @@ def _extract_deconv_worker(start_sample):
         - p.trough_offset,
     )
     for i in range(len(spike_index)):
-        resid[spike_index[i, 0] + rel_times] -= p.templates_up[
-            spike_train[i, 1]
-        ]
+        resid[spike_index[i, 0] + rel_times] -= (
+            scalings[:, None, None] * p.templates_up[spike_train[i, 1]]
+        )
 
     if p.save_residual:
         np.save(
@@ -286,7 +296,9 @@ def _extract_deconv_worker(start_sample):
             buffer=-start_sample + buffer_left + pad_left,
         )
         # now add the templates
-        waveforms += p.templates_loc[spike_train[:, 1]]
+        waveforms += (
+            scalings[:, None, None] * p.templates_loc[spike_train[:, 1]]
+        )
         if p.save_cleaned_waveforms:
             np.save(p.temp_dir / f"cleaned_{batch_str}.npy", waveforms)
 
@@ -380,6 +392,7 @@ def _extract_deconv_init(
     temp_dir,
     T_samples,
     templates_loc,
+    scalings,
 ):
     with h5py.File(subtraction_h5) as h5:
         tpca_mean = h5["tpca_mean"][:]
@@ -409,6 +422,7 @@ def _extract_deconv_init(
     _extract_deconv_worker.spike_length_samples = spike_length_samples
     _extract_deconv_worker.templates_up = templates_up
     _extract_deconv_worker.templates_loc = templates_loc
+    _extract_deconv_worker.scalings = scalings
     _extract_deconv_worker.temp_dir = temp_dir
     _extract_deconv_worker.T_samples = T_samples
     _extract_deconv_worker.batch_length = batch_length
