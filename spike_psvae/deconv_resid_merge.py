5e2849c9987c4a51a2b2c717fa176eba61a2dc26
julienboussard <jb4365@columbia.edu>
2022-11-20 09:55:52 -0500

    Update deconv_resid_merge.py

--- spike_psvae/deconv_resid_merge.py
+++ spike_psvae/deconv_resid_merge.py
@@ -400,12 +400,14 @@ def run_deconv_merge(
     deconv_threshold_mul=0.9,
     # 2 is conservative, 2.5 is nice, 3 is aggressive
     merge_resid_threshold=2.5,
+    tpca=None
 ):
     templates_cleaned, extra = get_templates(
         spike_train,
         geom,
         raw_binary_file,
         unit_max_channels,
+        tpca = tpca
     )
 
     # get rms on active channels
