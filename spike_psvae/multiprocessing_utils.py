2a2515e8723d25e15828ed30e4b240f5e32639e9
Charlie Windolf <cwindolf95@gmail.com>
2022-12-19 14:50:57 -0500

    Latest drifty deconv + extraction
    Former-commit-id: c71a752b669c27a54a4ebd0c0b95af42116384a7

--- spike_psvae/multiprocessing_utils.py
+++ spike_psvae/multiprocessing_utils.py
@@ -1,3 +1,7 @@
+from multiprocessing import get_context
+from concurrent.futures import ProcessPoolExecutor
+
+
 class MockFuture:
     """See below."""
 
@@ -18,9 +22,11 @@ class MockPoolExecutor:
         mp_context=None,
         initializer=None,
         initargs=None,
+        context=None,
     ):
         initializer(*initargs)
         self.map = map
+        self.imap = map
 
     def __enter__(self):
         return self
@@ -39,3 +45,9 @@ class MockQueue:
         self.q = []
         self.put = self.q.append
         self.get = lambda: self.q.pop(0)
+
+
+def get_pool(n_jobs, context="spawn", cls=ProcessPoolExecutor):
+    Executor = cls if (n_jobs and n_jobs > 1) else MockPoolExecutor
+    context = get_context(context)
+    return Executor, context
