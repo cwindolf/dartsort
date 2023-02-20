d53e67e87f26d2bdeb0045aba72820f9694028a8
Charlie Windolf <cwindolf95@gmail.com>
2023-02-20 14:06:37 -0500

    Update tests
    Former-commit-id: 7b792670dc08346afc6511d99ed43ce64d3cf54c

--- notebook/tests/test_relocation.py
+++ notebook/tests/test_relocation.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.14.0
+#       jupytext_version: 1.14.4
 #   kernelspec:
 #     display_name: Python [conda env:a]
 #     language: python
