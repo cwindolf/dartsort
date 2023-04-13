7a1bfea25d30d9bf7c784e1f7092d8119dcfa907
julienboussard <jb4365@columbia.edu>
2023-04-13 14:52:26 -0400

    Update grab_and_localize.py

--- spike_psvae/grab_and_localize.py
+++ spike_psvae/grab_and_localize.py
@@ -129,6 +129,9 @@ def _job(batch_start):
         device=p.device,
         denoiser=p.denoiser,
     )
+    
+    if torch.is_tensor(waveforms):
+        waveforms = waveforms.cpu().numpy()
 
     # -- localize and return
     ptps = waveforms.ptp(1)
