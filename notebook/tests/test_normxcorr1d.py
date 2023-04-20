7c4ebfd59531aef4735ce053b3a3ae3cf17df443
Charlie Windolf <cwindolf95@gmail.com>
2023-04-20 14:06:44 -0700

    Test extended normxcorr1d
    Former-commit-id: 4efd9fb1c93cba35a42eeb9e2a6aafcc2c239584

--- notebook/tests/test_normxcorr1d.py
+++ notebook/tests/test_normxcorr1d.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.14.4
+#       jupytext_version: 1.14.5
 #   kernelspec:
 #     display_name: Python [conda env:a]
 #     language: python
@@ -24,12 +24,17 @@ from spike_psvae.ibme_corr import normxcorr1d
 import torch.nn.functional as F
 
 # %%
+import matplotlib.pyplot as plt
+
+# %%
 ones = torch.ones(11)
 up = torch.arange(11.)
 tri = 5 - torch.abs(up - 5)
 tri
 
 # %%
+tri2 = torch.cat((tri, tri))
+tri2.shape
 
 # %%
 normxcorr1d(up[None], up[None])
@@ -44,6 +49,15 @@ normxcorr1d(up[None], up[None], weights=tri)
 normxcorr1d(tri[None], tri[None], weights=tri)
 
 # %%
+plt.plot(normxcorr1d(tri[None], tri[None], weights=tri).squeeze())
+
+# %%
 normxcorr1d(torch.arange(22.).reshape(2, 11), up[None], weights=tri)
+
+# %%
+normxcorr1d(tri[None], tri2[None], weights=tri)
+
+# %%
+plt.plot(normxcorr1d(tri[None], tri2[None], weights=tri).squeeze())
 
 # %%
