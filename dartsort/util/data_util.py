448d4e1b48e2927fafee76770699046aa7835d59
Charlie Windolf <cwindolf95@gmail.com>
2023-07-07 16:34:54 -0400

    Un-break the module

--- dartsort/util/data_util.py
+++ dartsort/util/data_util.py
@@ -4,8 +4,11 @@ SpikeDataset = namedtuple("SpikeDataset", ["name", "shape_per_spike", "dtype"])
 
 
 class SpikeTrain:
+    pass
 
 
 class H5FeatureExtractor:
-    def __init__(self, hdf5_filename, require_datasets=["times", "amplitude_vectors"]):
+    def __init__(
+        self, hdf5_filename, require_datasets=["times", "amplitude_vectors"]
+    ):
         pass
