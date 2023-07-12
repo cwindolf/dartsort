0dfeec2924e28c7a89725812dd431948d3bafbd1
Charlie Windolf <cwindolf95@gmail.com>
2023-07-12 16:07:10 -0400

    Add SubtractionPeeler.from_config

--- dartsort/main.py
+++ dartsort/main.py
@@ -36,38 +36,12 @@ def subtract(
     show_progress=True,
     device=None,
 ):
-    # construct denoising and featurization pipelines
-    subtraction_denoising_pipeline = WaveformPipeline.from_config(
-        subtraction_config.subtraction_denoising_config
-    )
-    featurization_pipeline = WaveformPipeline.from_config(featurization_config)
-
-    # waveform extraction channel neighborhoods
-    channel_index = make_channel_index(
-        recording.get_channel_locations(), subtraction_config.extract_radius
-    )
-    # per-threshold spike event deduplication channel neighborhoods
-    spatial_dedup_channel_index = make_channel_index(
-        recording.get_channel_locations(),
-        subtraction_config.spatial_dedup_radius,
-    )
-
-    subtraction_peeler = SubtractionPeeler(
+    subtraction_peeler = SubtractionPeeler.from_config(
         recording,
-        channel_index,
-        subtraction_denoising_pipeline,
-        featurization_pipeline,
-        trough_offset_samples=subtraction_config.trough_offset_samples,
-        spike_length_samples=subtraction_config.spike_length_samples,
-        detection_thresholds=subtraction_config.detection_thresholds,
-        chunk_length_samples=subtraction_config.chunk_length_samples,
-        peak_sign=subtraction_config.peak_sign,
-        spatial_dedup_channel_index=spatial_dedup_channel_index,
-        n_seconds_fit=subtraction_config.n_seconds_fit,
-        fit_subsampling_random_state=0,
+        subtraction_config=subtraction_config,
+        featurization_config=featurization_config,
         device=device,
     )
-
     subtraction_peeler.peel(
         output_hdf5_filename,
         chunk_starts_samples=chunk_starts_samples,
@@ -76,5 +50,4 @@ def subtract(
         residual_filename=residual_filename,
         show_progress=show_progress,
     )
-
     return DARTsortSorting.from_peeling_hdf5(output_hdf5_filename)
