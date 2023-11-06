c665a44e0ad6a060d668e69015b657eabf7901f7
julien <jb4365@columbia.edu>
2023-11-06 11:52:49 -0500

    dipole localization
    Former-commit-id: 21ea5925521670e01493fa494de3ef28281d9e7a

--- src/dartsort/transform/localize.py
+++ src/dartsort/transform/localize.py
@@ -63,3 +63,65 @@ class PointSourceLocalization(BaseWaveformFeaturizer):
             ]
         )
         return localizations
+
+class DipoleLocalization(BaseWaveformFeaturizer):
+    """Order of output columns: x, y, z_abs, alpha"""
+
+    default_name = "dipole_localizations"
+    shape = (4,)
+    dtype = torch.double
+
+    def __init__(
+        self,
+        channel_index,
+        geom,
+        radius=None,
+        n_channels_subset=None,
+        logbarrier=True,
+        amplitude_kind="peak",
+        model="dipole",
+        name=None,
+        name_prefix="",
+    ):
+        assert amplitude_kind in ("peak", "ptp")
+        super().__init__(
+            geom=geom,
+            channel_index=channel_index,
+            name=name,
+            name_prefix=name_prefix,
+        )
+        self.amplitude_kind = amplitude_kind
+        self.radius = radius
+        self.n_channels_subset = n_channels_subset
+        self.logbarrier = logbarrier
+        self.model = model
+
+    def transform(self, waveforms, max_channels=None):
+        # get amplitude vectors
+        if self.amplitude_kind == "peak":
+            ampvecs = waveforms.abs().max(dim=1).values
+        elif self.amplitude_kind == "ptp":
+            ampvecs = ptp(waveforms, dim=1)
+
+        with torch.enable_grad():
+            loc_result = localize_amplitude_vectors(
+                ampvecs,
+                self.geom,
+                max_channels,
+                channel_index=self.channel_index,
+                radius=self.radius,
+                n_channels_subset=self.n_channels_subset,
+                logbarrier=self.logbarrier,
+                model=self.model,
+                dtype=self.dtype,
+            )
+
+        localizations = torch.column_stack(
+            [
+                loc_result["x"],
+                loc_result["y"],
+                loc_result["z_abs"],
+                loc_result["alpha"],
+            ]
+        )
+        return localizations
