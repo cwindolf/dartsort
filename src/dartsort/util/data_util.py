4e1f1bd2e6975e92389f36bbec31e6e432bb5e5c
Charlie Windolf <cwindolf95@gmail.com>
2023-11-13 12:51:29 -0800

    Return spike count
    Former-commit-id: 1032f7447982cfeafc750886abea5d9aea507722

--- src/dartsort/util/data_util.py
+++ src/dartsort/util/data_util.py
@@ -149,6 +149,7 @@ def check_recording(
         dedup_channel_index = make_channel_index(
             rec.get_channel_locations(), dedup_spatial_radius
         )
+    failed = False
 
     # run detection and compute spike detection rate and data range
     spike_rates = []
@@ -173,6 +174,7 @@ def check_recording(
             "you experience memory issues.",
             RuntimeWarning,
         )
+        failed = True
 
     if max_abs > expected_value_range:
         warn(
@@ -180,5 +182,6 @@ def check_recording(
             "check that your data has been preprocessed, including standardization.",
             RuntimeWarning,
         )
+        failed = True
 
-    return avg_detections_per_second, max_abs
+    return failed, avg_detections_per_second, max_abs
