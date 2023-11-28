144388b5c952004010d2cca19399a2b5c2cab75f
Charlie Windolf <c.windolf@columbia.edu>
2023-11-28 17:38:05 -0500

    Sorta functional
    Former-commit-id: 713e6288e4765b6b7675325666235c1bb4b4d6b3

--- src/dartsort/util/drift_util.py
+++ src/dartsort/util/drift_util.py
@@ -220,13 +220,9 @@ def get_spike_pitch_shifts(
     else:
         probe_displacement = registered_depths_um - depths_um
 
-    print(f"get_spike_pitch_shifts {pitch=}")
-    print(f"get_spike_pitch_shifts {probe_displacement.min()=} {probe_displacement.max()=}")
-
     # if probe_displacement > 0, then the registered position is below the original
     # and, to be conservative, round towards 0 rather than using //
     n_pitches_shift = (probe_displacement / pitch).astype(int)
-    print(f"get_spike_pitch_shifts {n_pitches_shift.min()=} {n_pitches_shift.max()=}")
 
     return n_pitches_shift
 
@@ -547,15 +543,6 @@ def get_shift_and_unit_pairs(
         reg_depths_um = np.concatenate((reg_depths_um_a, reg_depths_um_b))
 
     # figure out all shifts for all units at all times
-    print(f"get_shift_and_unit_pairs {np.min(chunk_time_centers_s)=} {np.max(chunk_time_centers_s)=}")
-    print(f"get_shift_and_unit_pairs {reg_depths_um.min()=} {reg_depths_um.max()=}")
-    print(f"get_shift_and_unit_pairs {reg_depths_um_a.shape=} {reg_depths_um_b.shape=}")
-    print(f"get_shift_and_unit_pairs {reg_depths_um_a.min()=} {reg_depths_um_a.max()=}")
-    print(f"get_shift_and_unit_pairs {reg_depths_um_b.min()=} {reg_depths_um_b.max()=}")
-    print(f"get_shift_and_unit_pairs {motion_est.time_bin_centers_s.min()=}")
-    print(f"get_shift_and_unit_pairs {motion_est.time_bin_centers_s.max()=}")
-    print(f"get_shift_and_unit_pairs {motion_est.spatial_bin_centers_um.min()=}")
-    print(f"get_shift_and_unit_pairs {motion_est.spatial_bin_centers_um.max()=}")
     unreg_depths_um = np.stack(
         [
             invert_motion_estimate(
@@ -565,11 +552,8 @@ def get_shift_and_unit_pairs(
         ],
         axis=0,
     )
-    print(f"get_shift_and_unit_pairs {reg_depths_um.shape=} {unreg_depths_um.shape=}")
     assert unreg_depths_um.shape == (len(chunk_time_centers_s), len(reg_depths_um))
     diff = reg_depths_um - unreg_depths_um
-    print(f"get_shift_and_unit_pairs {unreg_depths_um.min()=} {unreg_depths_um.max()=}")
-    print(f"get_shift_and_unit_pairs {diff.min()=} {diff.max()=}")
     pitch_shifts = get_spike_pitch_shifts(
         depths_um=reg_depths_um,
         pitch=get_pitch(geom),
@@ -580,9 +564,8 @@ def get_shift_and_unit_pairs(
     else:
         shifts_a = pitch_shifts[:, :na]
         shifts_b = pitch_shifts[:, na:]
-    print(f"get_shift_and_unit_pairs {shifts_a.min()=} {shifts_a.max()=}")
-    print(f"get_shift_and_unit_pairs {shifts_b.min()=} {shifts_b.max()=}")
-    print(f"get_shift_and_unit_pairs {shifts_a.shape=} {shifts_b.shape=}")
+    print(f"{shifts_a.min()=} {shifts_a.max()=}")
+    print(f"{shifts_b.min()=} {shifts_b.max()=}")
 
     # assign ids to pitch/shift pairs
     template_shift_index_a = TemplateShiftIndex.from_shift_matrix(shifts_a)
--- src/dartsort/util/spiketorch.py
+++ src/dartsort/util/spiketorch.py
@@ -289,7 +289,7 @@ def depthwise_oaconv1d(input, weight, f2=None, padding=0):
     s2 = weight.shape[1]
     assert s1 >= s2
 
-    shape_final = s1 + s2 - 1
+    # shape_full = s1 + s2 - 1
     block_size, overlap, in1_step, in2_step = _calc_oa_lens(s1, s2)
     nstep1, pad1, nstep2, pad2 = steps_and_pad(
         s1, in1_step, s2, in2_step, block_size, overlap
@@ -320,15 +320,11 @@ def depthwise_oaconv1d(input, weight, f2=None, padding=0):
 
     oa = fold_res.reshape(n1, fold_out_len)
     # this is the full convolution
-    print(f"oaconv orig {oa.shape=}")
     # oa = oa[:, : shape_final]
-    print(f"oaconv full {oa.shape=}")
     # extract correct padding
     valid_len = s1 - s2 + 1
     valid_start = s2 - 1
     assert valid_start >= padding
     oa = oa[:, valid_start - padding:valid_start + valid_len + padding]
-    print(f"oaconv {oa.shape=} {valid_len=} {valid_start=} {padding=}")
-    print(f"oaconv {(valid_start - padding)=} {(valid_start + valid_len + padding)=}")
 
     return oa
