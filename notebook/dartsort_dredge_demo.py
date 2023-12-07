756b3176923dc552b8f65a14c1c50a401bcea941
Cole Hurwitz <churwitz@pcn-7-07.cm.cluster>
2023-12-07 08:53:53 -0800

    configs passed through properly now
    Former-commit-id: b2a689e6a8acaf6b40fab0836003bebe7b1081b3

--- notebook/dartsort_dredge_demo.py
+++ notebook/dartsort_dredge_demo.py
@@ -5,11 +5,11 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.14.7
+#       jupytext_version: 1.15.2
 #   kernelspec:
-#     display_name: Python [conda env:a]
+#     display_name: Python [conda env:dartsort]
 #     language: python
-#     name: conda-env-a-py
+#     name: conda-env-dartsort-py
 # ---
 
 # %%
@@ -54,15 +54,15 @@ import matplotlib.pyplot as plt
 
 # %%
 # path to the place where dataset1 was downloaded
-dataset1_path = Path("/mnt/3TB/dataset1/")
+dataset1_path = Path("/mnt/sdceph/users/churwitz/spike_sorting/dataset1/")
 
 # path where you'd like to store output files
-output_path = Path("/home/charlie/data/dataset1_demo_output")
+output_path = Path("/mnt/sdceph/users/churwitz/spike_sorting/dataset1/dataset1_output")
 
 # optional cache directory for storing a preprocessed binary
 # file. you can choose not to do this
 save_preprocessed_data = True
-cache_dir = Path("/home/charlie/scratch/dataset1_cache")
+cache_dir = Path("/mnt/sdceph/users/churwitz/spike_sorting/dataset1/dataset1_cache")
 
 # %%
 output_path.mkdir(exist_ok=True)
@@ -71,7 +71,6 @@ assert dataset1_path.exists()
 # %%
 # load up the NP2 dataset from dataset1
 rec = si.read_spikeglx(dataset1_path / "p2", stream_id="imec0.ap")
-
 if save_preprocessed_data and cache_dir.exists():
     rec = si.read_binary_folder(cache_dir)
 else:
@@ -89,7 +88,12 @@ else:
         rec = rec.save(folder=cache_dir, n_jobs=5, chunk_duration="1s")
 
 # %%
-plt.imshow(rec.get_traces(0, 0, 1000).T, aspect="auto")
+#get a snippet for testing
+num_seconds = 10*60
+sub_rec = rec.frame_slice(0, int(rec.get_sampling_frequency()*num_seconds))
+
+# %%
+plt.imshow(sub_rec.get_traces(0, 0, 1000).T, aspect="auto")
 plt.colorbar(label="amplitude (su)")
 plt.xlabel("time (samples)")
 plt.ylabel("channel");
@@ -101,7 +105,7 @@ plt.ylabel("channel");
 # this runs on GPU by default if you have one, and you can set n_jobs
 # higher if you have a nice one. this was run on a 1080 with 8GB ram,
 # and it used about all of the memory
-initial_detections, subtraction_h5 = subtract(rec, output_path, n_jobs=2)
+initial_detections, subtraction_h5 = subtract(sub_rec, output_path, n_jobs=2)
 
 # %%
 # load up extra data including spike locations
@@ -113,7 +117,9 @@ initial_detections
 dartvis.scatter_spike_features(subtraction_h5);
 
 # %%
-geom = rec.get_channel_locations()
+geom = sub_rec.get_channel_locations()
+
+# %%
 
 # %%
 # registration
@@ -140,3 +146,36 @@ mu.plot_me_traces(motion_est, plt.gca(), color="r");
 dartvis.scatter_time_vs_depth(subtraction_h5, registered=True, motion_est=motion_est);
 
 # %%
+from dartsort.main import cluster
+from dartsort.config import (FeaturizationConfig, ClusteringConfig, MatchingConfig,
+                             SubtractionConfig, TemplateConfig)
+
+clustering_config = ClusteringConfig(cluster_strategy='hdbscan', 
+                                     ensemble_strategy='forward_backward',
+                                     chunk_size_s=100,
+                                    )
+sorting = cluster(subtraction_h5, sub_rec, motion_est=motion_est, clustering_config=clustering_config)
+
+# %%
+figure = plt.figure(figsize=(12,6))
+dartvis.scatter_spike_features(subtraction_h5, 
+                               sorting=sorting, 
+                               figure=figure
+                              );
+                               # x_range_um=(-40, 60),
+                               # y_range_um=(3200, 3300)
+
+# %%
+from spike_psvae.cluster_viz import array_scatter
+
+x = initial_detections.point_source_localizations[:, 0]
+z = initial_detections.point_source_localizations[:, 2]
+a = initial_detections.denoised_amplitudes
+t = initial_detections.times_seconds
+z_reg = motion_est.correct_s(t, z)
+array_scatter(sorting.labels, geom, x, z_reg, a, zlim=(-100, 3000), do_ellipse=True, xlim=(-50, 92), ptplim = (0, 50));
+
+# %%
+zlim_list = [(-100, 500), (400, 1000), (900, 1500), (1400, 2000), (1900, 2500), (2400, 3000)]
+for zlim in zlim_list:
+    array_scatter(sorting.labels, geom, x, z_reg, a, zlim=zlim, do_ellipse=True, xlim=(-60, 100), ptplim = (0, 50));
