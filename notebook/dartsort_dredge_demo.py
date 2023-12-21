89ff92af587c824c03c09bd3ac2326cd8160132a
Charlie Windolf <cwindolf95@gmail.com>
2023-12-21 11:26:50 -0500

    Pull and fix tests

--- notebook/dartsort_dredge_demo.py
+++ notebook/dartsort_dredge_demo.py
@@ -5,11 +5,11 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.15.2
+#       jupytext_version: 1.14.7
 #   kernelspec:
-#     display_name: Python [conda env:dartsort]
+#     display_name: Python [conda env:a]
 #     language: python
-#     name: conda-env-dartsort-py
+#     name: conda-env-a-py
 # ---
 
 # %%
@@ -54,15 +54,15 @@ import matplotlib.pyplot as plt
 
 # %%
 # path to the place where dataset1 was downloaded
-dataset1_path = Path("/mnt/sdceph/users/churwitz/spike_sorting/dataset1/")
+dataset1_path = Path("/mnt/3TB/dataset1/")
 
 # path where you'd like to store output files
-output_path = Path("/mnt/sdceph/users/churwitz/spike_sorting/dataset1/dataset1_output")
+output_path = Path("/home/charlie/data/dataset1_demo_output")
 
 # optional cache directory for storing a preprocessed binary
 # file. you can choose not to do this
 save_preprocessed_data = True
-cache_dir = Path("/mnt/sdceph/users/churwitz/spike_sorting/dataset1/dataset1_cache")
+cache_dir = Path("/home/charlie/scratch/dataset1_cache")
 
 # %%
 output_path.mkdir(exist_ok=True)
@@ -71,6 +71,7 @@ assert dataset1_path.exists()
 # %%
 # load up the NP2 dataset from dataset1
 rec = si.read_spikeglx(dataset1_path / "p2", stream_id="imec0.ap")
+
 if save_preprocessed_data and cache_dir.exists():
     rec = si.read_binary_folder(cache_dir)
 else:
@@ -88,12 +89,7 @@ else:
         rec = rec.save(folder=cache_dir, n_jobs=5, chunk_duration="1s")
 
 # %%
-#get a snippet for testing
-num_seconds = 10*60
-sub_rec = rec.frame_slice(0, int(rec.get_sampling_frequency()*num_seconds))
-
-# %%
-plt.imshow(sub_rec.get_traces(0, 0, 1000).T, aspect="auto")
+plt.imshow(rec.get_traces(0, 0, 1000).T, aspect="auto")
 plt.colorbar(label="amplitude (su)")
 plt.xlabel("time (samples)")
 plt.ylabel("channel");
@@ -105,7 +101,7 @@ plt.ylabel("channel");
 # this runs on GPU by default if you have one, and you can set n_jobs
 # higher if you have a nice one. this was run on a 1080 with 8GB ram,
 # and it used about all of the memory
-initial_detections, subtraction_h5 = subtract(sub_rec, output_path, n_jobs=2)
+initial_detections, subtraction_h5 = subtract(rec, output_path, n_jobs=2)
 
 # %%
 # load up extra data including spike locations
@@ -117,9 +113,7 @@ initial_detections
 dartvis.scatter_spike_features(subtraction_h5);
 
 # %%
-geom = sub_rec.get_channel_locations()
-
-# %%
+geom = rec.get_channel_locations()
 
 # %%
 # registration
@@ -146,36 +140,3 @@ mu.plot_me_traces(motion_est, plt.gca(), color="r");
 dartvis.scatter_time_vs_depth(subtraction_h5, registered=True, motion_est=motion_est);
 
 # %%
-from dartsort.main import cluster
-from dartsort.config import (FeaturizationConfig, ClusteringConfig, MatchingConfig,
-                             SubtractionConfig, TemplateConfig)
-
-clustering_config = ClusteringConfig(cluster_strategy='hdbscan', 
-                                     ensemble_strategy='forward_backward',
-                                     chunk_size_s=100,
-                                    )
-sorting = cluster(subtraction_h5, sub_rec, motion_est=motion_est, clustering_config=clustering_config)
-
-# %%
-figure = plt.figure(figsize=(12,6))
-dartvis.scatter_spike_features(subtraction_h5, 
-                               sorting=sorting, 
-                               figure=figure
-                              );
-                               # x_range_um=(-40, 60),
-                               # y_range_um=(3200, 3300)
-
-# %%
-from spike_psvae.cluster_viz import array_scatter
-
-x = initial_detections.point_source_localizations[:, 0]
-z = initial_detections.point_source_localizations[:, 2]
-a = initial_detections.denoised_amplitudes
-t = initial_detections.times_seconds
-z_reg = motion_est.correct_s(t, z)
-array_scatter(sorting.labels, geom, x, z_reg, a, zlim=(-100, 3000), do_ellipse=True, xlim=(-50, 92), ptplim = (0, 50));
-
-# %%
-zlim_list = [(-100, 500), (400, 1000), (900, 1500), (1400, 2000), (1900, 2500), (2400, 3000)]
-for zlim in zlim_list:
-    array_scatter(sorting.labels, geom, x, z_reg, a, zlim=zlim, do_ellipse=True, xlim=(-60, 100), ptplim = (0, 50));
--- tests/test_matching.py
+++ tests/test_matching.py
@@ -163,7 +163,9 @@ def test_tiny_up(tmp_path, up_factor=8):
     times, channels, labels, upsampling_indices = np.array(tclu).reshape(-1, 4).T
     rec = np.zeros((recording_length_samples, n_channels), dtype="float32")
     for t, l, u in zip(times, labels, upsampling_indices):
-        temp = cupts.compressed_upsampled_templates[cupts.compressed_upsampling_map[l, u]]
+        temp = cupts.compressed_upsampled_templates[
+            cupts.compressed_upsampling_map[l, u]
+        ]
         print(f"{t=} {temp.ptp(0).max()=}")
         rec[
             t - trough_offset_samples : t - trough_offset_samples + spike_length_samples
@@ -205,7 +207,9 @@ def test_tiny_up(tmp_path, up_factor=8):
         ptps=template_data.templates.ptp(1).max(1),
         max_upsample=up_factor,
     )
-    assert np.array_equal(matcher.compressed_upsampled_temporal, tempup.compressed_upsampled_templates)
+    assert np.array_equal(
+        matcher.compressed_upsampled_temporal, tempup.compressed_upsampled_templates
+    )
     assert np.array_equal(matcher.objective_spatial_components, lrt.spatial_components)
     assert np.array_equal(matcher.objective_singular_values, lrt.singular_values)
     assert np.array_equal(matcher.spatial_components, lrt.spatial_components)
@@ -230,28 +234,29 @@ def test_tiny_up(tmp_path, up_factor=8):
             conv_filt = torch.bmm(torch.as_tensor(ssb[None]), template_a.mT)
             conv_filt = conv_filt[:, None]  # (nco, 1, rank, t)
             conv_in = torch.as_tensor(tempupb[None]).mT[None]
-            pconv_ = F.conv2d(
-                conv_in, conv_filt, padding=(0, 120), groups=1
-            )
+            pconv_ = F.conv2d(conv_in, conv_filt, padding=(0, 120), groups=1)
             pconv1 = pconv_.squeeze()[spike_length_samples - 1].numpy(force=True)
             assert torch.isclose(pcf, pconv_).all()
 
-            pconv2 = F.conv2d(
-                torch.as_tensor(templates[ia])[None, None],
-                torch.as_tensor(tupb)[None, None],
-            ).squeeze().numpy(force=True)
+            pconv2 = (
+                F.conv2d(
+                    torch.as_tensor(templates[ia])[None, None],
+                    torch.as_tensor(tupb)[None, None],
+                )
+                .squeeze()
+                .numpy(force=True)
+            )
             assert np.isclose(pconv2, tc)
             assert np.isclose(pc, tc)
             assert np.isclose(pconv1, pc)
             print(f" - {ia=} {ib=} {up=} {pc=} {tc=}")
-    
-    
+
     res = matcher.peel_chunk(
         torch.from_numpy(rec.get_traces().copy()),
         return_residual=True,
         return_conv=True,
     )
-    
+
     print(f'{res["n_spikes"]=} {len(times)=}')
     print(f"{cupts.compressed_upsampled_templates.ptp(1).max(1)=}")
     print(f'{res["collisioncleaned_waveforms"].numpy(force=True).ptp(1).max(1)=}')
@@ -350,7 +355,9 @@ def static_tester(tmp_path, up_factor=1):
         ptps=template_data.templates.ptp(1).max(1),
         max_upsample=up_factor,
     )
-    assert np.array_equal(matcher.compressed_upsampled_temporal, tempup.compressed_upsampled_templates)
+    assert np.array_equal(
+        matcher.compressed_upsampled_temporal, tempup.compressed_upsampled_templates
+    )
     assert np.array_equal(matcher.objective_spatial_components, lrt.spatial_components)
     assert np.array_equal(matcher.objective_singular_values, lrt.singular_values)
     assert np.array_equal(matcher.spatial_components, lrt.spatial_components)
@@ -375,16 +382,18 @@ def static_tester(tmp_path, up_factor=1):
             conv_filt = torch.bmm(torch.as_tensor(ssb[None]), template_a.mT)
             conv_filt = conv_filt[:, None]  # (nco, 1, rank, t)
             conv_in = torch.as_tensor(tempupb[None]).mT[None]
-            pconv_ = F.conv2d(
-                conv_in, conv_filt, padding=(0, 120), groups=1
-            )
+            pconv_ = F.conv2d(conv_in, conv_filt, padding=(0, 120), groups=1)
             pconv1 = pconv_.squeeze()[spike_length_samples - 1].numpy(force=True)
             assert torch.isclose(pcf, pconv_).all()
 
-            pconv2 = F.conv2d(
-                torch.as_tensor(templates[ia])[None, None],
-                torch.as_tensor(tupb)[None, None],
-            ).squeeze().numpy(force=True)
+            pconv2 = (
+                F.conv2d(
+                    torch.as_tensor(templates[ia])[None, None],
+                    torch.as_tensor(tupb)[None, None],
+                )
+                .squeeze()
+                .numpy(force=True)
+            )
             assert np.isclose(pconv2, tc)
             assert np.isclose(pc, tc)
             assert np.isclose(pconv1, pc)
@@ -414,210 +423,31 @@ def test_static_noup(tmp_path):
 
 
 def test_static_up(tmp_path):
-    static_tester(tmp_path, up_factor=4)
-
-
-def drifting_tester(tmp_path, up_factor=1):
-    recording_length_samples = 40_011
-    n_channels = 2
-    geom = np.c_[np.zeros(2), np.arange(2)]
-    geom
-
-    # template main channel traces
-    trace0 = 50 * np.exp(
-        -(((np.arange(spike_length_samples) - trough_offset_samples) / 10) ** 2)
-    )
-    trace1 = 250 * np.exp(
-        -(((np.arange(spike_length_samples) - trough_offset_samples) / 30) ** 2)
-    )
-
-    # templates
-    templates = np.zeros((3, spike_length_samples, n_channels), dtype="float32")
-    templates[0, :, 0] = trace0
-    templates[1, :, 0] = trace1
-    templates[2, :, 1] = trace0
-
-    # spike train
-    # fmt: off
-    tcl = [
-        100, 0, 0,
-        150, 0, 0,
-        151, 1, 2,
-        500, 0, 1,
-        2000, 0, 0,
-        2001, 0, 1,
-        25000, 1, 2,
-        25001, 0, 1,
-    ]
-    # fmt: on
-    times, channels, labels = np.array(tcl).reshape(-1, 3).T
-    rec = np.zeros((recording_length_samples, n_channels), dtype="float32")
-    for t, l in zip(times, labels):
-        rec[
-            t - trough_offset_samples : t - trough_offset_samples + spike_length_samples
-        ] += templates[l]
-    rec = si.NumpyRecording(rec, 30_000)
-    rec.set_dummy_probe_from_locations(geom)
-
-    template_config = config.TemplateConfig(
-        low_rank_denoising=False, superres_bin_min_spikes=0
-    )
-    template_data = TemplateData.from_config(
-        *no_overlap_recording_sorting(templates),
-        template_config,
-        motion_est=motion_util.IdentityMotionEstimate(),
-        n_jobs=0,
-        save_folder=tmp_path,
-        overwrite=True,
-    )
-
-    matcher = main.ObjectiveUpdateTemplateMatchingPeeler.from_config(
-        rec,
-        main.MatchingConfig(
-            threshold=0.01,
-            template_temporal_upsampling_factor=up_factor,
-            coarse_approx_error_threshold=0.0,
-            conv_ignore_threshold=0.0,
-            template_svd_compression_rank=2,
-        ),
-        nofeatcfg,
-        template_data,
-        motion_est=motion_util.IdentityMotionEstimate(),
-    )
-    matcher.precompute_peeling_data(tmp_path)
-
-    # tup = template_util.compressed_upsampled_templates(
-    #     template_data.templates, max_upsample=up_factor
-    # )
-    lrt = template_util.svd_compress_templates(
-        template_data.templates, rank=matcher.svd_compression_rank
-    )
-    tempup = template_util.compressed_upsampled_templates(
-        lrt.temporal_components,
-        ptps=template_data.templates.ptp(1).max(1),
-        max_upsample=up_factor,
-    )
-    print(f"{lrt.temporal_components.shape=}")
-    print(f"{lrt.singular_values.shape=}")
-    print(f"{lrt.spatial_components.shape=}")
-    assert np.array_equal(matcher.compressed_upsampled_temporal, tempup.compressed_upsampled_templates)
-    assert np.array_equal(matcher.objective_spatial_components, lrt.spatial_components)
-    assert np.array_equal(matcher.objective_singular_values, lrt.singular_values)
-    assert np.array_equal(matcher.spatial_components, lrt.spatial_components)
-    assert np.array_equal(matcher.singular_values, lrt.singular_values)
-    for up in range(up_factor):
-        ixa, ixb, pconv = matcher.pairwise_conv_db.query(
-            np.arange(3),
-            np.arange(3),
-            upsampling_indices_b=up + np.zeros(3, dtype=int),
-            grid=True,
-        )
-        centerpc = pconv[:, spike_length_samples - 1]
-        for ia, ib, pc, pcf in zip(ixa, ixb, centerpc, pconv):
-            # tupb = tup.compressed_upsampled_templates[
-            #     tup.compressed_upsampling_map[ib, up]
-            # ]
-            tempupb = tempup.compressed_upsampled_templates[
-                tempup.compressed_upsampling_map[ib, up]
-            ]
-            tupb = (tempupb * lrt.singular_values[ib]) @ lrt.spatial_components[ib]
-            tc = (templates[ia] * tupb).sum()
-
-            template_a = torch.as_tensor(templates[ia][None])
-            ssb = lrt.singular_values[ib][:, None] * lrt.spatial_components[ib]
-            conv_filt = torch.bmm(torch.as_tensor(ssb[None]), template_a.mT)
-            conv_filt = conv_filt[:, None]  # (nco, 1, rank, t)
-            conv_in = torch.as_tensor(tempupb[None]).mT[None]
-            pconv_ = F.conv2d(
-                conv_in, conv_filt, padding=(0, 120), groups=1
-            )
-            # print(f"{torch.abs(pcf - pconv_).max()=}")
-            pconv1 = pconv_.squeeze()[spike_length_samples - 1].numpy(force=True)
-            assert torch.isclose(pcf, pconv_).all()
-
-            pconv2 = F.conv2d(
-                torch.as_tensor(templates[ia])[None, None],
-                torch.as_tensor(tupb)[None, None],
-            ).squeeze().numpy(force=True)
-            assert np.isclose(pconv2, tc)
-
-            # print(f" - {ia=} {ib=}")
-            # print(f"   {pc=} {tc=} {pconv1=} {pconv2=}")
-            # print(f"   {pcf[120]=} {pcf[121]=} {pcf[122]=}")
-            # print(f" ~ {np.isclose(pc, tc)=}")
-            # print(f"   {np.isclose(pconv1, pc)=} {np.isclose(tc, pconv2)=}")
-            assert np.isclose(pc, tc)
-            assert np.isclose(pconv1, pc)
-
-    res = matcher.peel_chunk(
-        torch.from_numpy(rec.get_traces().copy()),
-        return_residual=True,
-        return_conv=True,
-    )
-
-    print()
-    print()
-    print(f"{len(times)=}")
-    print(f"{res['n_spikes']=}")
-    print()
-    print(f'{torch.square(res["residual"]).mean()=}')
-    print(f'{torch.abs(res["residual"]).max()=}')
-    print(f'{torch.square(res["conv"]).mean()=}')
-    print(f'{torch.abs(res["conv"]).max()=}')
-    print(f'{res["conv"].min()=} {res["conv"].max()=}')
-    tnsq = np.linalg.norm(templates, axis=(1, 2)) ** 2
-    print(f"{res['conv'].shape=} {tnsq.shape=}")
-    print(f'{(2*res["conv"] - tnsq[:,None]).max()=}')
-    print()
-    print(f'{res["times_samples"]=}')
-    print(f"{times=}")
-    print()
-    print(f'{res["labels"]=}')
-    print(f"{labels=}")
-    print()
-    print(f'{np.c_[res["times_samples"], res["labels"], res["upsampling_indices"]]=}')
-    print(f"{np.c_[times, labels]=}")
-    print()
-    print(f'{res["upsampling_indices"]=}')
-
-    assert res["n_spikes"] == len(times)
-    assert np.array_equal(res["times_samples"], times)
-    assert np.array_equal(res["labels"], labels)
-    print(f"{torch.square(res['residual']).mean()=}")
-    print(f"{torch.square(res['conv']).mean()=}")
-    assert np.isclose(
-        torch.square(res["residual"]).mean(),
-        0.0,
-    )
-    assert np.isclose(
-        torch.square(res["conv"]).mean(),
-        0.0,
-        atol=1e-4,
-    )
+    static_tester(tmp_path, up_factor=8)
 
 
 if __name__ == "__main__":
     import tempfile
     from pathlib import Path
 
-    print("\n"*5)
+    print("\n" * 5)
     print("test tiny")
     with tempfile.TemporaryDirectory() as tdir:
         test_tiny(Path(tdir))
 
-    print("\n"*5)
+    print("\n" * 5)
     print("test tiny_up")
     with tempfile.TemporaryDirectory() as tdir:
         test_tiny_up(Path(tdir))
 
     print()
-    print("\n"*5)
+    print("\n" * 5)
     print("test test_static_noup")
     with tempfile.TemporaryDirectory() as tdir:
         test_static_noup(Path(tdir))
 
     print()
-    print("\n"*5)
+    print("\n" * 5)
     print("test test_static_up")
     with tempfile.TemporaryDirectory() as tdir:
         test_static_up(Path(tdir))
