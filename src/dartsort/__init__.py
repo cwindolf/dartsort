7a0cdfaa1a7e6b7433f0b6e34222e633cf72ef85
Charlie Windolf <cwindolf95@gmail.com>
2024-01-18 14:00:09 -0500

    Wow, oops.

--- src/dartsort/__init__.py
+++ src/dartsort/__init__.py
@@ -1,10 +1,9 @@
-from config import *
-from main import (DARTsortSorting, ObjectiveUpdateTemplateMatchingPeeler,
-                  SubtractionPeeler, check_recording, cluster, dartsort, match,
-                  run_peeler, split_merge, subtract)
-
+from .config import *
 from .localize.localize_util import (localize_amplitude_vectors, localize_hdf5,
                                      localize_waveforms)
+from .main import (DARTsortSorting, ObjectiveUpdateTemplateMatchingPeeler,
+                   SubtractionPeeler, check_recording, cluster, dartsort,
+                   match, run_peeler, split_merge, subtract)
 from .peel.grab import GrabAndFeaturize
 from .transform import WaveformPipeline
-from .waveform_util import make_channel_index
+from .util.waveform_util import make_channel_index
