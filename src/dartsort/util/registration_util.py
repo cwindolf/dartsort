361662ba1db9addbb2ea0c3885eb7052db191776
Charlie Windolf <c.windolf@columbia.edu>
2024-02-02 14:28:40 -0500

    Debug registration
    Former-commit-id: ed200ef55d1dd81f3f66a2df27f9b418bf3da5bc

--- src/dartsort/util/registration_util.py
+++ src/dartsort/util/registration_util.py
@@ -26,6 +26,7 @@ def estimate_motion(
     window_margin_um: Optional[float] = None,
     max_dt_s: float = 0.1,
     max_disp_um: Optional[float] = None,
+    rigid=False,
     localizations_dataset_name="point_source_localizations",
     amplitudes_dataset_name="denoised_ptp_amplitudes",
     device=None,
@@ -42,14 +43,14 @@ def estimate_motion(
         raise ValueError("Please install DREDge to use motion estimation.")
 
     x = getattr(sorting, localizations_dataset_name)[:, 0]
-    z = getattr(sorting, localizations_dataset_name)[:, 1]
+    z = getattr(sorting, localizations_dataset_name)[:, 2]
     geom = recording.get_channel_locations()
     xmin = geom[:, 0].min() - probe_boundary_padding_um
     xmax = geom[:, 0].max() + probe_boundary_padding_um
     zmin = geom[:, 1].min() - probe_boundary_padding_um
     zmax = geom[:, 1].max() + probe_boundary_padding_um
-    xvalid = x == np.clip(x, xmin, xmax)
-    zvalid = z == np.clip(z, zmin, zmax)
+    xvalid = x == x.clip(xmin, xmax)
+    zvalid = z == z.clip(zmin, zmax)
     valid = np.flatnonzero(xvalid & zvalid)
 
     # features for registration
@@ -59,14 +60,15 @@ def estimate_motion(
 
     # run registration
     motion_est, info = dredge_ap.register(
-        a,
-        z,
-        t_s,
-        window_step_um=window_step_um,
+        amps=a,
+        depths_um=z,
+        times_s=t_s,
+        rigid=rigid,
         bin_um=spatial_bin_length_um,
         bin_s=temporal_bin_length_s,
-        window_scale_um=window_scale_um,
-        window_margin_um=window_margin_um,
+        win_step_um=window_step_um,
+        win_scale_um=window_scale_um,
+        win_margin_um=window_margin_um,
         max_disp_um=max_disp_um,
         max_dt_s=max_dt_s,
         device=device,
