87fb42087a328e51083238d1e0f9f2524ea27c0b
Charlie Windolf <cwindolf95@gmail.com>
2024-02-02 17:09:17 -0500

    Make it easier to change things like number of chans shown in plot
    Former-commit-id: 6b95686bc5f873c9909203f8b40cea520fc8a656

---
+++ src/dartsort/vis/main.py
@@ -0,0 +1,9 @@
+from . import scatterplots, unit
+
+
+def visualize_sorting_step(sorting, gt_sorting=None):
+    pass
+
+
+def visualize_all_sorting_steps(out_dir, gt_sorting=None, n_jobs=None):
+    pass
