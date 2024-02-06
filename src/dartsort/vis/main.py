72a8cf1f68cbfc6a94e3d387fcaed590e661bee1
Charlie Windolf <cwindolf95@gmail.com>
2024-02-06 14:15:18 -0500

    Scripting entry points and high level visualization routines

--- src/dartsort/vis/main.py
+++
@@ -1,9 +0,0 @@
-from . import scatterplots, unit
-
-
-def visualize_sorting_step(sorting, gt_sorting=None):
-    pass
-
-
-def visualize_all_sorting_steps(out_dir, gt_sorting=None, n_jobs=None):
-    pass
