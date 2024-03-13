a2f878007d4b4f8277aeae0088f185d898969217
Charlie Windolf <cwindolf95@gmail.com>
2024-03-13 08:26:40 -0700

    Get movies and ellipses working; templates over time logic; passing TSVDs into templates; sorting chunking logic; improved colors
    Former-commit-id: 9fc0d7a0d45c3129210eec00e2bb955e523482b7

--- src/dartsort/vis/over_time.py
+++ src/dartsort/vis/over_time.py
@@ -24,6 +24,7 @@ def sorting_scatter_animation(
     chunk_length_samples=None,
     show_template_main_traces=True,
     scatter_template_features=True,
+    suptitle=None,
     depth_feature="z",
     features=("x", "amplitude", "log_peak_to_trough"),
     template_config=basic_template_config,
@@ -32,7 +33,7 @@ def sorting_scatter_animation(
     n_jobs_templates=0,
     device=None,
     feature_getters=None,
-    interval=200,
+    interval=1500,
     figsize=(11, 8.5),
     dpi=150,
 ):
@@ -79,10 +80,27 @@ def sorting_scatter_animation(
         show_template_main_traces=show_template_main_traces,
         scatter_template_features=scatter_template_features,
         feature_getters=None,
+        suptitle=suptitle,
         interval=interval,
         figsize=figsize,
     )
-    anim.save(str(output_path), writer="ffmpeg", codec="libx264", dpi=dpi, bitrate=-1)
+    # messing with this to try to make the videos work
+    # with mac quicktime
+    anim.save(
+        str(output_path),
+        writer="ffmpeg",
+        codec="libx264",
+        extra_args=[
+            "-pix_fmt",
+            "yuv420p",
+            "-vf",
+            "pad=ceil(iw/2)*2:ceil(ih/2)*2",
+            # "-c:v", "hevc_videotoolbox",
+            # "-level:v", "5.1", "-profile:v", "High",
+        ],
+        dpi=dpi,
+        bitrate=-1,
+    )
     plt.close(fig)
 
 
@@ -94,7 +112,9 @@ usual_feature_getters = {
 }
 
 
-def get_template_features(template_data, feature_names, scatter_template_features):
+def get_template_features(
+    template_data, feature_names, scatter_template_features
+):
     if not scatter_template_features:
         return {}
 
@@ -118,11 +138,16 @@ def get_template_features(template_data, feature_names, scatter_template_feature
 def update_frame(
     axes,
     feature_scatters,
+    feature_ellips,
     template_scatters,
+    geom_scatter,
     chunk_time_range,
     chunk_sorting,
     all_spike_features,
+    geom,
+    motion_est,
     depth_feature="z",
+    semilog_amplitude=True,
     chunk_template_data=None,
     max_n_templates=None,
     chunk_template_features=None,
@@ -131,7 +156,11 @@ def update_frame(
     # main traces
     if show_template_main_traces and chunk_template_data is not None:
         axes[0].clear()
-        analysis_plots.scatter_max_channel_waveforms(axes[0], chunk_template_data, lw=1)
+        analysis_plots.scatter_max_channel_waveforms(
+            axes[0], chunk_template_data, lw=1
+        )
+        xmin, dx = geom[:, 0].min(), geom[:, 0].ptp()
+        axes[0].set_xlim([xmin - dx / 2, xmin + 3 * dx / 2])
         axes = axes[1:]
 
     spike_mask = np.equal(
@@ -141,32 +170,58 @@ def update_frame(
     non_depth_keys = [k for k in all_spike_features if k != depth_feature]
     for j, k in enumerate(non_depth_keys):
         # determine this chunk's spike mask
-        feature_scatters[k] = scatterplots.scatter_feature_vs_depth(
+        _, feature_scatters[k], feature_ellips[k] = scatterplots.scatter_feature_vs_depth(
             all_spike_features[k][spike_mask],
             all_spike_features[depth_feature][spike_mask],
+            ax=axes[j],
             labels=chunk_sorting.labels[spike_mask],
             pad_to_max=True,
             scat=feature_scatters.get(k, None),
             geom=chunk_template_data.registered_geom,
+            show_ellipses=True,
+            max_n_labels=max_n_templates,
+            ellip=feature_ellips.get(k, None),
         )
+
+        if k != "x":
+            xmin, xmax = (
+                all_spike_features[k].min(),
+                all_spike_features[k].max(),
+            )
+            margin = (xmax - xmin) * 0.025
+        else:
+            xmin, xmax = geom[:, 0].min(), geom[:, 0].max()
+            margin = 100
+            disp = motion_est.disp_at_s(sum(chunk_time_range) / 2, geom[:, 1], grid=True)
+            disp = np.c_[np.zeros_like(disp), disp]
+            if geom_scatter:
+                geom_scatter[0].set_offsets(geom - disp)
+            else:
+                geom_scatter.append(axes[j].scatter(*(geom - disp).T, s=5, marker="s", color="k", lw=0))
+
         if chunk_template_features is not None:
-            template_scatters[k] = scatterplots.scatter_feature_vs_depth(
+            _, template_scatters[k] = scatterplots.scatter_feature_vs_depth(
                 chunk_template_features[k],
                 chunk_template_features[depth_feature],
+                ax=axes[j],
                 labels=chunk_template_data.unit_ids,
                 pad_to_max=True,
                 max_spikes_plot=max_n_templates,
                 scat=template_scatters.get(k, None),
-                s=5,
+                s=15,
                 linewidth=1,
                 edgecolor="k",
+                marker="s",
                 geom=chunk_template_data.registered_geom,
                 rasterized=False,
             )
 
-        xmin, xmax = all_spike_features[k].min(), all_spike_features[k].max()
-        margin = (xmax - xmin) * 0.025
-        axes[k].set_xlim([xmin - margin, xmax + margin])
+        if k == "amplitude" and semilog_amplitude:
+            margin = 1.05
+            axes[j].set_xlim([xmin / margin, xmax * margin])
+            axes[j].semilogx()
+        else:
+            axes[j].set_xlim([xmin - margin, xmax + margin])
 
 
 def make_animation(
@@ -179,6 +234,7 @@ def make_animation(
     show_template_main_traces=True,
     scatter_template_features=True,
     feature_getters=None,
+    suptitle=None,
     interval=200,
     figsize=(11, 8.5),
 ):
@@ -189,17 +245,17 @@ def make_animation(
         with h5py.File(sorting_analysis.hdf5_path, "r", locking=False) as h5:
             all_spike_features = {
                 name: h5[feature_getters[name][0]][feature_getters[name][1]]
-                for name in feature_names
+                for name in list(feature_names) + [depth_feature]
             }
             if (
                 depth_feature in all_spike_features
                 and sorting_analysis.motion_est is not None
             ):
-                all_spike_features[depth_feature] = (
-                    sorting_analysis.motion_est.correct_s(
-                        h5["times_seconds", :],
-                        depths_um=all_spike_features[depth_feature],
-                    )
+                all_spike_features[
+                    depth_feature
+                ] = sorting_analysis.motion_est.correct_s(
+                    t_s=h5["times_seconds"][()],
+                    depth_um=all_spike_features[depth_feature],
                 )
     else:
         all_spike_features = {}
@@ -208,7 +264,11 @@ def make_animation(
     max_n_templates = max(t.templates.shape[0] for t in chunk_templates)
     if chunk_templates is not None and scatter_template_features:
         all_template_features = [
-            get_template_features(chunk_td, feature_names, scatter_template_features)
+            get_template_features(
+                chunk_td,
+                list(feature_names) + [depth_feature],
+                scatter_template_features,
+            )
             for chunk_td in chunk_templates
         ]
 
@@ -218,17 +278,24 @@ def make_animation(
         nrows=1, ncols=ncols, figsize=figsize, layout="constrained", sharey=True
     )
     feature_scatters = {}
+    feature_ellips = {}
     template_scatters = {}
+    geom_scatter = []
 
     def closure(frame):
         j = frame
+        print(f"animation {frame=}")
         update_frame(
             axes,
             feature_scatters,
+            feature_ellips,
             template_scatters,
+            geom_scatter,
             chunk_time_ranges_s[j],
             chunk_sortings[j],
             all_spike_features,
+            sorting_analysis.recording.get_channel_locations(),
+            sorting_analysis.motion_est,
             depth_feature=depth_feature,
             chunk_template_data=chunk_templates[j],
             max_n_templates=max_n_templates,
@@ -238,6 +305,10 @@ def make_animation(
 
     # initialize
     closure(0)
+    if suptitle is not None:
+        fig.suptitle(suptitle)
+    for j, k in enumerate(feature_names):
+        axes[j + show_template_main_traces].set_xlabel(k)
 
     # run
     anim = animation.FuncAnimation(
