5df9206b009a6e27dc77fc835ee3ee720cc821fb
Julien Boussard <jboussard@exp-1-19.expanse.sdsc.edu>
2024-07-11 18:54:57 -0700

    add template loc to cluster_scatter

--- src/spike_psvae/cluster_viz.py
+++ src/spike_psvae/cluster_viz.py
@@ -359,6 +359,7 @@ def array_scatter(
     scatter_temp_amp_z=False,
     template_data=None,
     com_x_temp=None,
+    units_to_mark=None,
 ):
     fig = None
     if axes is None:
@@ -382,7 +383,10 @@ def array_scatter(
         do_ellipse=do_ellipse,
     )
     if scatter_temp_amp_z and template_data is not None and com_x_temp is not None:
-        axes[0].scatter(com_x_temp, template_data.registered_template_depths_um, marker="x", c = "red", s=15)
+        if units_to_mark is None:
+            axes[0].scatter(com_x_temp, template_data.registered_template_depths_um, marker="x", c = "red", s=25)
+        else:
+            axes[0].scatter(com_x_temp[units_to_mark], template_data.registered_template_depths_um[units_to_mark], marker="x", c = "red", s=25)
 
     axes[0].scatter(*geom.T, c="orange", marker="s", s=10)
     axes[0].scatter(geom[0, 0], geom[0, 1], c="orange", marker="s", s=10, label='Channel Locations')
@@ -404,7 +408,10 @@ def array_scatter(
     )
     if scatter_temp_amp_z and template_data is not None:
         ptp_temps =  50*np.log(5+template_data.templates.ptp(1).max(1))
-        axes[1].scatter(ptp_temps, template_data.registered_template_depths_um, marker="x", c = "red", s=15)
+        if units_to_mark is None:
+            axes[1].scatter(ptp_temps, template_data.registered_template_depths_um, marker="x", c = "red", s=25)
+        else:
+            axes[1].scatter(ptp_temps[units_to_mark], template_data.registered_template_depths_um[units_to_mark], marker="x", c = "red", s=25)
         # for u in range(len(ptp_temps)):
         #     plt.text(ptp_temps + 0.2, template_data.registered_template_depths_um + 1, f"{u}")
     axes[1].set_xlabel("Amplitude (s.u.)", fontsize=16)
