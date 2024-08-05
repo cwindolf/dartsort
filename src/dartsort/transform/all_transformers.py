1d5088112d0f8b2bce6b7301e64c607c89a5de2c
Charlie Windolf <c.windolf@columbia.edu>
2024-08-05 17:26:56 -0400

    Bones of combined featurizer+denoiser
    Former-commit-id: d03c38dace50b42c4cc6ea812d3b563358f2b39e

--- src/dartsort/transform/all_transformers.py
+++ src/dartsort/transform/all_transformers.py
@@ -2,7 +2,7 @@ from .amplitudes import AmplitudeFeatures, AmplitudeVector, MaxAmplitude
 from .enforce_decrease import EnforceDecrease
 from .localize import Localization, PointSourceLocalization
 from .single_channel_denoiser import SingleChannelWaveformDenoiser
-from .temporal_pca import TemporalPCADenoiser, TemporalPCAFeaturizer
+from .temporal_pca import TemporalPCADenoiser, TemporalPCAFeaturizer, TemporalPCA
 from .transform_base import Waveform
 
 all_transformers = [
@@ -16,6 +16,7 @@ all_transformers = [
     Localization,
     PointSourceLocalization,
     AmplitudeFeatures,
+    TemporalPCA,
 ]
 
 transformers_by_class_name = {cls.__name__: cls for cls in all_transformers}
