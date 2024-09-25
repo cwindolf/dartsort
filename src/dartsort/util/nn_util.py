f22e1ba9afde539e83c795fa04ed3b2c566efeaa
Charlie Windolf <cwindolf95@gmail.com>
2024-09-25 14:06:40 -0700

    Lates NN stuff + clustering vis debugging + GT metrics

--- src/dartsort/util/nn_util.py
+++ src/dartsort/util/nn_util.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch.nn.functional as F
 
 
 def get_mlp(input_dim, hidden_dims, output_dim, use_batchnorm=True):
@@ -15,3 +16,36 @@ def get_mlp(input_dim, hidden_dims, output_dim, use_batchnorm=True):
     layers.append(nn.Linear(final_dim, output_dim))
 
     return nn.Sequential(*layers)
+
+
+def get_waveform_mlp(
+    spike_length_samples,
+    n_input_channels,
+    hidden_dims,
+    output_dim,
+    input_includes_mask=True,
+    use_batchnorm=True,
+    channelwise_dropout_p=0.0,
+):
+    input_dim = n_input_channels * (spike_length_samples + input_includes_mask)
+
+    layers = []
+    if channelwise_dropout_p:
+        layers.append(ChannelwiseDropout(channelwise_dropout_p))
+    layers.append(nn.Flatten())
+    layers.append(get_mlp(input_dim, hidden_dims, output_dim, use_batchnorm=use_batchnorm))
+    return nn.Sequential(*layers)
+
+
+class ChannelwiseDropout(nn.Module):
+
+    def __init__(self, p):
+        super().__init__()
+        self.p = p
+
+    def forward(self, waveforms):
+        return F.dropout1d(
+            waveforms.permute(0, 2, 1),
+            p=self.p,
+            training=self.training,
+        ).permute(0, 2, 1)
