7d097c4fbf0e65d921bdc9434dfe5cb0cc342d5b
Charlie Windolf <cwindolf95@gmail.com>
2024-11-01 14:35:03 -0700

    Serde error + layernorm nnloc

--- src/dartsort/transform/all_transformers.py
+++ src/dartsort/transform/all_transformers.py
@@ -7,7 +7,7 @@ from .decollider import Decollider
 from .enforce_decrease import EnforceDecrease
 from .localize import Localization, PointSourceLocalization
 from .pipeline import WaveformPipeline
-from .single_channel_denoiser import SingleChannelWaveformDenoiser
+from .single_channel_denoiser import SingleChannelWaveformDenoiser, SingleChannelDenoiser
 from .temporal_pca import (TemporalPCA, TemporalPCADenoiser,
                            TemporalPCAFeaturizer)
 from .transform_base import Passthrough, Waveform
@@ -33,5 +33,5 @@ transformers_by_class_name = {cls.__name__: cls for cls in all_transformers}
 
 # serialization
 if hasattr(torch.serialization, 'add_safe_globals'):
-    others = [WaveformPipeline, set, torch.nn.ModuleList, slice, torch.nn.Sequential]
+    others = [WaveformPipeline, set, torch.nn.ModuleList, SingleChannelDenoiser, slice, torch.nn.Sequential]
     torch.serialization.add_safe_globals(all_transformers + others)
