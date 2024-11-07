19bce7365327766b16883dcf0a69d17b4a79382c
Charlie Windolf <cwindolf95@gmail.com>
2024-11-07 10:03:06 -0800

    Declare that line of code as suspicious and needing an eye kept on

--- src/dartsort/cluster/modes.py
+++ src/dartsort/cluster/modes.py
@@ -108,6 +108,9 @@ def smoothed_dipscore_at(
     null="isotoniconesideunnormed",
     debug_info=None,
 ):
+    if samples.size < 3:
+        return 0.0
+
     if sample_weights is None:
         sample_weights = np.ones_like(samples)
     densities = density.get_smoothed_densities(
@@ -121,7 +124,13 @@ def smoothed_dipscore_at(
     )
     spacings = np.diff(samples)
     spacings = np.concatenate((spacings[:1], 0.5 * (spacings[1:] + spacings[:-1]), spacings[-1:]))
-    densities /= (densities * spacings).sum()
+    try:
+        with np.errstate(all="raise"):
+            densities /= (densities * spacings).sum()
+    except Exception as e:
+        print(f"{spacings.shape=} {spacings.min()=} {spacings.max()=}")
+        print(f"{densities.shape=} {densities.min()=} {densities.max()=}")
+        return np.inf
 
     if cut is None:
         # closest maxes left + right of 0
