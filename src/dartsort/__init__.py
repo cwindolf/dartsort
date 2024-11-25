95efeb4d5957b5f778fbb12af8a7a915bc3a0383
Charlie Windolf <charliexwindolf@gmail.com>
2024-11-25 13:11:03 +0000

    Export more things

--- src/dartsort/__init__.py
+++ src/dartsort/__init__.py
@@ -1,24 +1,25 @@
 import importlib.metadata
 
+from . import util
+from .cluster import density, merge, postprocess, ppcalib
+from .cluster.gaussian_mixture import GaussianUnit, SpikeMixtureModel
+from .cluster.initial import initial_clustering
+from .cluster.stable_features import (SpikeFeatures, SpikeNeighborhoods,
+                                      StableSpikeDataset)
 from .config import *
 from .localize.localize_util import (localize_amplitude_vectors, localize_hdf5,
                                      localize_waveforms)
 from .main import (ObjectiveUpdateTemplateMatchingPeeler, SubtractionPeeler,
-                   check_recording, cluster, dartsort, match, run_peeler,
-                   split_merge, subtract, estimate_motion)
+                   check_recording, cluster, dartsort, estimate_motion, match,
+                   run_peeler, split_merge, subtract)
 from .peel.grab import GrabAndFeaturize
 from .templates import TemplateData
 from .transform import WaveformPipeline
+from .util import data_util, noise_util, spiketorch, waveform_util
 from .util.analysis import DARTsortAnalysis
 from .util.data_util import DARTsortSorting
 from .util.drift_util import registered_geometry
-from .util.waveform_util import make_channel_index
-from .cluster import merge, postprocess, density
-from . import util
-from .util import noise_util, data_util, waveform_util, spiketorch
 from .util.noise_util import EmbeddedNoise
-from .cluster.initial import initial_clustering
-from .cluster.stable_features import StableSpikeDataset
-from .cluster.gaussian_mixture import SpikeMixtureModel, GaussianUnit
+from .util.waveform_util import make_channel_index
 
 __version__ = importlib.metadata.version("dartsort")
