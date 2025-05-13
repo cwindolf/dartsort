3454d0a69d6909a84cbcad60b61fc455b18b3dff
Charlie Windolf <charliexwindolf@gmail.com>
2025-05-13 11:42:12 -0400

    tests: Use fixture rec in threshold

--- tests/test_threshold.py
+++ tests/test_threshold.py
@@ -1,27 +1,14 @@
 import tempfile
-from pathlib import Path
 
 import dartsort
-from dartsort.util import simkit, noise_util
 
 
-def test_sim():
-    geom = simkit.generate_geom()
-    rec_sim = simkit.SimulatedRecording(
-        duration_samples=3 * 30_000,
-        n_units=40,
-        template_simulator=simkit.PointSource3ExpSimulator(geom),
-        noise=noise_util.WhiteNoise(len(geom)),
-        min_fr_hz=20.0,
-        max_fr_hz=31.0,
-        temporal_jitter=4,
-    )
+def test_sim(sim_recordings):
+    rec = sim_recordings["static"]["rec"]
+    gt_st = sim_recordings["static"]["sorting"]
 
     with tempfile.TemporaryDirectory() as tempdir:
-        test_h5 = Path(tempdir) / "sim.h5"
-        rec = rec_sim.simulate(test_h5)
         st = dartsort.threshold(
             recording=rec, output_dir=dartsort.resolve_path(tempdir)
         )
-        st0 = dartsort.DARTsortSorting.from_peeling_hdf5(test_h5)
-        assert abs(len(st) - len(st0)) / len(st0) < 0.2
+        assert abs(len(st) - len(gt_st)) / len(gt_st) < 0.2
