matching: Bug in pconv computation

src/dartsort/peel/matching_util/pairwise_util.py
@@ -674,13 +674,6 @@ def correlate_pairs_lowrank(
     should contain lots of duplicates, since they are already representing
     pairs.
 
-    Templates Ka = Sa Ta, Kb = Sb Tb. The channel-summed convolution is
-        (Ka (*) Kb) = sum_c Ka(c) * Kb(c)
-                    = (Sb.T @ Ka) (*) Tb
-                    = (Sb.T @ Sa @ Ta) (*) Tb
-    where * is cross-correlation, and (*) is channel (or rank) summed.
-    We use full-height conv2d to do rank-summed convs.
-
     Returns
     -------
     pconv, kept
@@ -708,6 +701,8 @@ def correlate_pairs_lowrank(
         max_shift = 0
     elif max_shift == "same":
         max_shift = t // 2
+    else:
+        assert isinstance(max_shift, int)
 
     # batch over n_pairs for memory reasons
     pconv = torch.zeros(
@@ -717,6 +712,7 @@ def correlate_pairs_lowrank(
     )
     for istart in range(0, n_pairs, batch_size):
         iend = min(istart + batch_size, n_pairs)
+        nbatch = iend - istart
         ix = slice(istart, iend)
 
         spatial_a_ = spatial_a[ix_a[conv_ix][ix]]
@@ -728,22 +724,17 @@ def correlate_pairs_lowrank(
             spatial_a_ = spatial_a_ * active[:, None, :]
             spatial_b_ = spatial_b_ * active[:, None, :]
 
-        # want conv filter: nco, 1, rank, t
-        template_a = torch.bmm(temporal_a[ix_a[conv_ix][ix]], spatial_a_)
-        conv_filt = torch.bmm(spatial_b_, template_a.mT)
-        conv_filt = conv_filt[:, None]  # (nco, 1, rank, t)
+        ta = temporal_a[ix_a[conv_ix][ix]]
+        tb = temporal_b[conv_compressed_upsampled_ix_b[ix]]
+        assert ta.shape == tb.shape == (nbatch, t, rank)
 
-        # 1, nco, rank, t
-        conv_in = temporal_b[conv_compressed_upsampled_ix_b[ix]].mT[None]
-
-        # conv2d:
-        # depthwise, chans=nco. batch=1. h=rank. w=t. out: nup=1, nco, 1, 2p+1.
-        # input (conv_in): nup, nco, rank, t.
-        # filters (conv_filt): nco, 1, rank, t. (groups=nco).
-        pconv_ = F.conv2d(
-            conv_in, conv_filt, padding=(0, max_shift), groups=iend - istart
+        # # we want to convolve templates A and B. have to do rank^2
+        # because the basis is not shared.
+        sb_ta_sa = torch.einsum(
+            "ntr,nrc,nqc->rnqt", temporal_a[ix_a[conv_ix][ix]], spatial_a_, spatial_b_
         )
-        pconv[istart:iend] = pconv_[0, :, 0, :]  # nco, nup, time
+        pc = F.conv2d(sb_ta_sa, tb.mT[:, None], groups=nbatch, padding=(0, max_shift))
+        pconv[istart:iend] = torch.flip(pc.sum(0)[:, 0], dims=(1,))
 
     # more stringent covisibility
     if conv_ignore_threshold is not None:

tests/test_matching.py
@@ -25,8 +25,8 @@ nofeatcfg = dartsort.FeaturizationConfig(
 spike_length_samples = 121
 trough_offset_samples = 42
 
-RES_ATOL = 1e-5
-CONV_ATOL = 0.1
+RES_ATOL = 1e-10
+CONV_ATOL = 1e-4
 
 
 @pytest.mark.parametrize("scaling", [0.0, 0.01])
@@ -248,26 +248,44 @@ def test_tiny_up(tmp_path, up_factor, scaling, cd_iter, up_offset):
                 tupb = (tempupb * lrt.singular_values[ib]) @ lrt.spatial_components[ib]
                 tc = (templates[ia] * tupb).sum()
 
-                template_a = torch.as_tensor(templates[ia][None])
+                # test against a very direct way of computing this convolution
+                # just directly create the actual templates
+                temp_a = torch.as_tensor(templates[ia])
+                print(f"{temp_a.shape=}")
                 ssb = lrt.singular_values[ib][:, None] * lrt.spatial_components[ib]
-                conv_filt = torch.bmm(torch.as_tensor(ssb[None]), template_a.mT)
-                conv_filt = conv_filt[:, None]  # (nco, 1, rank, t)
-                conv_in = torch.as_tensor(tempupb[None]).mT[None]
-                pconv_ = F.conv2d(conv_in, conv_filt, padding=(0, 120), groups=1)
-                pconv1 = pconv_.squeeze()[spike_length_samples - 1].numpy(force=True)
-                assert torch.isclose(pcf, pconv_).all()
+                print(f"{ssb.shape=} {tempupb.shape=}")
+                temp_b = torch.as_tensor(tempupb) @ (torch.as_tensor(ssb))
+                print(f"{temp_b.shape=}")
 
-                pconv2 = (
-                    F.conv2d(
-                        torch.as_tensor(templates[ia])[None, None],
-                        torch.as_tensor(tupb)[None, None],
-                    )
-                    .squeeze()
-                    .numpy(force=True)
+                pconv2 = F.conv2d(
+                    temp_a[None, None], temp_b[None, None], padding=(temp_a.shape[0] - 1, 0)
                 )
-                assert np.isclose(pconv2, tc)
+                print(f"{pconv2.shape=}")
+                print(f"{pconv.shape=}")
+                print(f"{pc.shape=}")
+                pconv2 = pconv2[0, 0, :, 0]
+
+
+                # template_a = torch.as_tensor(templates[ia][None])
+                # ssb = lrt.singular_values[ib][:, None] * lrt.spatial_components[ib]
+                # conv_filt = torch.bmm(torch.as_tensor(ssb[None]), template_a.mT)
+                # conv_filt = conv_filt[:, None]  # (nco, 1, rank, t)
+                # conv_in = torch.as_tensor(tempupb[None]).mT[None]
+                # pconv_ = F.conv2d(conv_in, conv_filt, padding=(0, 120), groups=1)
+                # pconv1 = pconv_.squeeze()[spike_length_samples - 1].numpy(force=True)
+                # assert torch.isclose(pcf, pconv_).all()
+
+                # pconv2 = (
+                #     F.conv2d(
+                #         torch.as_tensor(templates[ia])[None, None],
+                #         torch.as_tensor(tupb)[None, None],
+                #     )
+                #     .squeeze()
+                #     .numpy(force=True)
+                # )
+                assert np.allclose(pconv2.numpy()[::-1], pconv)
                 assert np.isclose(pc, tc)
-                assert np.isclose(pconv1, pc)
+                # assert np.isclose(pconv1, pc)
 
         res = matcher.peel_chunk(
             torch.from_numpy(rec.get_traces().copy()),
@@ -341,14 +359,14 @@ def test_static(tmp_path, up_factor, cd_iter):
             overwrite=True,
         )
         matching_cfg = dartsort.MatchingConfig(
-                threshold=0.01,
-                template_temporal_upsampling_factor=up_factor,
-                amplitude_scaling_variance=0.0,
-                coarse_approx_error_threshold=0.0,
-                conv_ignore_threshold=0.0,
-                template_svd_compression_rank=2,
-                cd_iter=cd_iter,
-            )
+            threshold=0.01,
+            template_temporal_upsampling_factor=up_factor,
+            amplitude_scaling_variance=0.0,
+            coarse_approx_error_threshold=0.0,
+            conv_ignore_threshold=0.0,
+            template_svd_compression_rank=2,
+            cd_iter=cd_iter,
+        )
 
         matcher = dartsort.ObjectiveUpdateTemplateMatchingPeeler.from_config(
             rec,