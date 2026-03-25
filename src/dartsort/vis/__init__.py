e72a88f0a108373f1e687f0e24883877a38dc594
Charlie Windolf <charliexwindolf@gmail.com>
2026-03-25 17:57:12 -0400

    dep error in dartsort.vis

--- src/dartsort/vis/__init__.py
+++ src/dartsort/vis/__init__.py
@@ -1,18 +1,26 @@
+try:
+    import seaborn
+except ImportError:
+    raise ImportError(
+        'seaborn isn\'t installed; pip install "dartsort[vis]" or "dartsort[full]" to '
+        "get the dartsort.vis dependencies."
+    )
+
 from .analysis_plots import *
+from .colors import *
+from .gt import *
+from .mixture import (
+    MixtureVisData,
+    fit_mixture_and_visualize_all_components,
+    fit_mixture_for_vis,
+    make_mixture_component_summary,
+    make_mixture_summaries,
+)
+from .recanim import RecordingAnimation
 from .scatterplots import *
 from .sorting import *
 from .unit import *
+from .unit_comparison import *
+from .versus import *
 from .vismain import *
 from .waveforms import *
-from .gt import *
-from .versus import *
-from .unit_comparison import *
-from .colors import *
-from .recanim import RecordingAnimation
-from .mixture import (
-    make_mixture_component_summary,
-    make_mixture_summaries,
-    MixtureVisData,
-    fit_mixture_for_vis,
-    fit_mixture_and_visualize_all_components,
-)
